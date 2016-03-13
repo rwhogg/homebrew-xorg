3a132f46b4236a770a35b1b9694cbdcbd678bb0d
Maxim Belkin <maxim.belkin@gmail.com>
2016-03-12 22:13:22 -0600

    xcb-util-keysyms: new formula

---
+++ xcb-util-keysyms.rb
@@ -0,0 +1,28 @@
+class XcbUtilKeysyms < Formula
+  desc "Additional extensions to the XCB library."
+  homepage "http://xcb.freedesktop.org"
+  url "http://xcb.freedesktop.org/dist/xcb-util-keysyms-0.4.0.tar.bz2"
+  sha256 "0ef8490ff1dede52b7de533158547f8b454b241aa3e4dcca369507f66f216dd9"
+
+  option "with-static", "Build static libraries"
+
+  depends_on "pkg-config" => :build
+  depends_on "libxcb"
+  depends_on "libxdmcp"   => :run
+  depends_on "libxau"     => :run
+
+
+  def install
+        args = %W[
+      --prefix=#{prefix}
+      --sysconfdir=#{etc}
+      --localstatedir=#{var}
+    ]
+	  args << "--disable-static" if !build.with?("static")
+
+    system "./configure", *args
+    system "make"
+    system "make", "install"
+  end
+
+end
