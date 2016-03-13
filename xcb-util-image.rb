2f4162dd81294499cfb269b1679e85f6eed41dc2
Maxim Belkin <maxim.belkin@gmail.com>
2016-03-12 21:33:11 -0600

    xcb-util-image: new formula

---
+++ xcb-util-image.rb
@@ -0,0 +1,29 @@
+class XcbUtilImage < Formula
+  desc "Additional extensions to the XCB library."
+  homepage "http://xcb.freedesktop.org"
+  url "http://xcb.freedesktop.org/dist/xcb-util-image-0.4.0.tar.bz2"
+  sha256 "2db96a37d78831d643538dd1b595d7d712e04bdccf8896a5e18ce0f398ea2ffc"
+
+  option "with-static", "Build static libraries"
+
+  depends_on "pkg-config" => :build
+  depends_on "libxcb"
+  depends_on "xcb-util"
+  depends_on "libxdmcp"   => :run
+  depends_on "libxau"     => :run
+
+
+  def install
+        args = %W[
+      --prefix=#{prefix}
+      --sysconfdir=#{etc}
+      --localstatedir=#{var}
+    ]
+	  args << "--disable-static" if !build.with?("static")
+
+    system "./configure", *args
+    system "make"
+    system "make", "install"
+  end
+
+end
