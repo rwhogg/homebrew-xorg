1be391c7c98b9d529d4486e96b4f69c36b64de93
Maxim Belkin <maxim.belkin@gmail.com>
2016-03-18 10:42:01 -0500

    fontsproto: enable-specs option, xmlto patch, documentation dependencies

--- fontsproto.rb
+++ fontsproto.rb
@@ -5,13 +5,23 @@ class Fontsproto < Formula
   sha256 "259046b0dd9130825c4a4c479ba3591d6d0f17a33f54e294b56478729a6e5ab8"
   # tag "linuxbrew"
 
+  option "with-specs",  "Build specifications"
+
   depends_on "pkg-config"         =>  :build
   depends_on "util-macros"        =>  :build
-  depends_on "xorg-sgml-doctools" => [:build, :recommended]
-  depends_on "fop"                => [:build, :optional]
-  depends_on "libxslt"            => [:build, :optional]
-  depends_on "xmlto"              => [:build, :optional]
-  depends_on "asciidoc"           => [:build, :optional]
+
+  # Patch for xmlto
+  patch do
+    url "https://raw.githubusercontent.com/Linuxbrew/homebrew-xorg/master/patch_configure.diff"
+    sha256 "e3aff4be9c8a992fbcbd73fa9ea6202691dd0647f73d1974ace537f3795ba15f"
+  end
+
+  if build.with?("specs")
+    depends_on "xmlto"   => :build
+    depends_on "fop"     => [:build, :recommended]
+    depends_on "libxslt" => [:build, :recommended]
+    depends_on "xorg-sgml-doctools" => [:build, :recommended]
+  end
 
   def install
     args = %W[
@@ -21,6 +31,9 @@ class Fontsproto < Formula
       --disable-dependency-tracking
       --disable-silent-rules
     ]
+
+    # Be explicit about the configure flags
+    args << "--enable-specs=#{build.with?("specs") ? "yes" : "no"}"
 
     system "./configure", *args
     system "make", "install"
