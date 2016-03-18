c1288b6bfd1f1a0da818f0bdfef8bb0a3659037f
Maxim Belkin <maxim.belkin@gmail.com>
2016-03-18 11:09:47 -0500

    scrnsaverproto: with-specs option, xmlto patch, configure flag

--- scrnsaverproto.rb
+++ scrnsaverproto.rb
@@ -5,13 +5,23 @@ class Scrnsaverproto < Formula
   sha256 "8bb70a8da164930cceaeb4c74180291660533ad3cc45377b30a795d1b85bcd65"
   # tag "linuxbrew"
 
+  option "with-specs",  "Build specifications"
+
   depends_on "pkg-config"         =>  :build
   depends_on "util-macros"        =>  :build
-  depends_on "xorg-sgml-doctools" => [:build, :recommended]
-  depends_on "fop"                => [:build, :optional]
-  depends_on "libxslt"            => [:build, :optional]
-  depends_on "xmlto"              => [:build, :optional]
-  depends_on "asciidoc"           => [:build, :optional]
+
+  # Patch for xmlto
+  patch do
+    url "https://raw.githubusercontent.com/Linuxbrew/homebrew-xorg/master/patch_configure.diff"
+    sha256 "e3aff4be9c8a992fbcbd73fa9ea6202691dd0647f73d1974ace537f3795ba15f"
+  end
+
+  if build.with?("specs")
+    depends_on "xmlto"   => :build
+    depends_on "fop"     => [:build, :recommended]
+    depends_on "libxslt" => [:build, :recommended]
+    depends_on "xorg-sgml-doctools" => [:build, :recommended]
+  end
 
   def install
     args = %W[
@@ -21,6 +31,9 @@ class Scrnsaverproto < Formula
       --disable-dependency-tracking
       --disable-silent-rules
     ]
+
+    # Be explicit about the configure flags
+    args << "--enable-specs=#{build.with?("specs") ? "yes" : "no"}"
 
     system "./configure", *args
     system "make", "install"
