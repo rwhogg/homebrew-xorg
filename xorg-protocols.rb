d9e1a3c2bd535f1b21c2298413ba0fd4ddde7c15
Maxim Belkin <maxim.belkin@gmail.com>
2016-03-18 12:35:01 -0500

    xorg-protocols: removed extra options

--- xorg-protocols.rb
+++ xorg-protocols.rb
@@ -6,23 +6,21 @@ class XorgProtocols < Formula
   sha256   "846477dd316964fe52e9f73c3752d3c0383f7099046dd06ddfda92c2f9baad5d"
   # tag "linuxbrew"
 
-  depends_on "xorg-sgml-doctools" => [:build, :recommended]
-  depends_on "fop"                => [:build, :optional]
-  depends_on "libxslt"            => [:build, :optional]
-  depends_on "xmlto"              => [:build, :optional]
-  depends_on "asciidoc"           => [:build, :optional]
+  option "with-specs",  "Build specifications"
+  args = build.with?("specs") ? "with-specs" : ""
 
-  args = %W[]
-  args << "without-xorg-sgml-doctools" if build.without?("xorg-sgml-doctools")
-  args << "with-fop"                   if build.with?("fop")
-  args << "with-libxslt"               if build.with?("libxslt")
-  args << "with-xmlto"                 if build.with?("xmlto")
-  args << "with-asciidoc"              if build.with?("asciidoc")
+  # with specs
+  res1 = %w[bigreqsproto fontsproto inputproto kbproto recordproto scrnsaverproto xcmiscproto xextproto xproto]
 
-  res = %w[bigreqsproto compositeproto damageproto dmxproto dri2proto dri3proto fixesproto fontsproto glproto inputproto kbproto presentproto randrproto recordproto renderproto resourceproto scrnsaverproto videoproto xcmiscproto xextproto xf86bigfontproto xf86dgaproto xf86driproto xf86vidmodeproto xineramaproto xproto]
+  # without specs
+  res2 = %w[compositeproto damageproto dmxproto dri2proto dri3proto fixesproto glproto presentproto randrproto renderproto resourceproto videoproto xf86bigfontproto xf86dgaproto xf86driproto xf86vidmodeproto xineramaproto]
 
-  res.each do |r|
+  res1.each do |r|
     depends_on r => args
+  end
+
+  res2.each do |r|
+    depends_on r
   end
 
   def install
