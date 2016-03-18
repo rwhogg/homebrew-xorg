827d4967435a32667c0fa74b73fde0599a185a5f
Maxim Belkin <maxim.belkin@gmail.com>
2016-03-18 03:22:46 -0500

    libxmu: configure flags + patch for xmlto

--- libxmu.rb
+++ libxmu.rb
@@ -7,14 +7,27 @@ class Libxmu < Formula
 
   option "with-check",  "Run a check before install"
   option "with-static", "Build static libraries"
+  option "with-docs",   "Build documentation"
 
   depends_on "pkg-config" =>  :build
-  depends_on "fontconfig" =>  :build
 
   depends_on "libxt"
   depends_on "libxext"
   depends_on "libx11"
   depends_on "xextproto" =>  :build
+
+  # Patch for xmlto
+  patch do
+    url "https://raw.githubusercontent.com/Linuxbrew/homebrew-xorg/master/patch_configure.diff"
+    sha256 "e3aff4be9c8a992fbcbd73fa9ea6202691dd0647f73d1974ace537f3795ba15f"
+  end
+
+  if build.with?("docs")
+    depends_on "xmlto"   => :build
+    depends_on "fop"     => [:build, :recommended]
+    depends_on "libxslt" => [:build, :recommended]
+    depends_on "xorg-sgml-doctools" => [:build, :recommended]
+  end
 
   def install
     args = %W[
@@ -24,7 +37,10 @@ class Libxmu < Formula
       --disable-dependency-tracking
       --disable-silent-rules
     ]
-	  args << "--disable-static" if !build.with?("static")
+
+    # Be explicit about the configure flags
+    args << "--enable-static=#{build.with?("static") ? "yes" : "no"}"
+    args << "--enable-docs=#{build.with?("docs") ? "yes" : "no"}"
 
     system "./configure", *args
     system "make"
