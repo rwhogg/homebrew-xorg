0ded86f0d091fd25e56e2a1f3b9ba830070a77c6
Bob W. Hogg <rwhogg@linux.com>
2016-03-17 21:34:53 -0700

    xf86driproto: additional configure flags

--- xf86driproto.rb
+++ xf86driproto.rb
@@ -18,6 +18,8 @@ class Xf86driproto < Formula
       --prefix=#{prefix}
       --sysconfdir=#{etc}
       --localstatedir=#{var}
+      --disable-dependency-tracking
+      --disable-silent-rules
     ]
 
     system "./configure", *args
