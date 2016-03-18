85903c3e270c17afc10836a85635592896cc9962
Maxim Belkin <maxim.belkin@gmail.com>
2016-03-18 01:22:50 -0500

    libx11: option for specs, patch for xmlto, explicit dependencies, no fontconfig dep

--- libx11.rb
+++ libx11.rb
@@ -7,14 +7,28 @@ class Libx11 < Formula
 
   option "with-check",  "Run a check before install"
   option "with-static", "Build static libraries"
+  option "with-specs",  "Build specifications"
 
   depends_on "pkg-config" =>  :build
-  depends_on "fontconfig" =>  :build
   depends_on "xextproto"  =>  :build
   depends_on "xtrans"     =>  :build
   depends_on "libxcb"
   depends_on "kbproto"    =>  :build
   depends_on "inputproto" =>  :build
+
+  # Patch for xmlto
+  patch do
+    url "https://raw.githubusercontent.com/Linuxbrew/homebrew-xorg/master/patch_configure.diff"
+    sha256 "e3aff4be9c8a992fbcbd73fa9ea6202691dd0647f73d1974ace537f3795ba15f"
+  end
+
+  if build.with?("specs")
+    depends_on "xmlto"   => :build
+    depends_on "fop"     => [:build, :recommended]
+    depends_on "libxslt" => [:build, :recommended]
+    depends_on "perl"    => [:build, :optional]
+    depends_on "xorg-sgml-doctools" => [:build, :recommended]
+  end
 
   def install
     args = %W[
@@ -24,7 +38,10 @@ class Libx11 < Formula
       --disable-dependency-tracking
       --disable-silent-rules
     ]
-	  args << "--disable-static" if !build.with?("static")
+
+    # Be explicit about the configure flags
+    args << "--enable-static=#{build.with?("static") ? "yes" : "no"}"
+    args << "--enable-specs=#{build.with?("specs") ? "yes" : "no"}"
 
     system "./configure", *args
     system "make"
