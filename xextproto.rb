c43f6c69717a232f4d24563c232ef29871740046
Maxim Belkin <maxim.belkin@gmail.com>
2016-03-18 11:43:21 -0500

    xextproto: with-specs option, xmlto patch, configure flags

--- xextproto.rb
+++ xextproto.rb
@@ -5,13 +5,23 @@ class Xextproto < Formula
   sha256 "f3f4b23ac8db9c3a9e0d8edb591713f3d70ef9c3b175970dd8823dfc92aa5bb0"
   # tag "linuxbrew"
 
+  option "with-specs",  "Build specifications"
+
   depends_on "pkg-config"         =>  :build
   depends_on "util-macros"        =>  :build
-  depends_on "xorg-sgml-doctools" => [:build, :recommended]
-  depends_on "fop"                => [:build, :optional]
-  depends_on "libxslt"            => [:build, :optional]
-  depends_on "xmlto"              => [:build, :optional]
-  depends_on "asciidoc"           => [:build, :optional]
+
+  # Patch for xmlto
+  patch do
+    url "https://raw.githubusercontent.com/Linuxbrew/homebrew-xorg/master/patch_configure.diff"
+    sha256 "e3aff4be9c8a992fbcbd73fa9ea6202691dd0647f73d1974ace537f3795ba15f"
+  end
+
+  if build.with?("specs")
+    depends_on "xmlto"   => :build
+    depends_on "fop"     => [:build, :recommended]
+    depends_on "libxslt" => [:build, :recommended]
+    depends_on "xorg-sgml-doctools" => [:build, :recommended]
+  end
 
   def install
     args = %W[
@@ -21,6 +31,9 @@ class Xextproto < Formula
       --disable-dependency-tracking
       --disable-silent-rules
     ]
+
+    # Be explicit about the configure flags
+    args << "--enable-specs=#{build.with?("specs") ? "yes" : "no"}"
 
     system "./configure", *args
     system "make", "install"
