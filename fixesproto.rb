e30796cc9e626b973c1e1d09b7722cede1f7f515
Bob W. Hogg <rwhogg@linux.com>
2016-05-06 08:46:50 -0400

    fixesproto: add 5.0 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-xorg#28.
    Signed-off-by: Bob W. Hogg <rwhogg@linux.com>

--- fixesproto.rb
+++ fixesproto.rb
@@ -5,6 +5,11 @@ class Fixesproto < Formula
   sha256 "ba2f3f31246bdd3f2a0acf8bd3b09ba99cab965c7fb2c2c92b7dc72870e424ce"
   # tag "linuxbrew"
 
+  bottle do
+    cellar :any
+    sha256 "dcfed30e21197c8d4c9384e9d1e5304c49983d4866f0186ebe2fdf06e80a4801" => :x86_64_linux
+  end
+
   depends_on "pkg-config"         =>  :build
   depends_on "util-macros"        =>  :build
 
