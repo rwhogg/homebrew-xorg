7ca9bc16ccdda4a5d54463abef7a16fa1ffe9579
Maxim Belkin <maxim.belkin@gmail.com>
2016-05-06 11:08:36 -0500

    xorg.rb: remastered

--- xorg.rb
+++ xorg.rb
@@ -1,61 +1,78 @@
 class Xorg < Formula
   desc "X.Org X11 libraries"
-  homepage "http://www.x.org/" ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7lib.html
+  homepage "http://www.x.org/"
+  ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7lib.html
   url "https://raw.githubusercontent.com/Linuxbrew/homebrew-xorg/master/README.md"
   version  "20160314"
   sha256   "846477dd316964fe52e9f73c3752d3c0383f7099046dd06ddfda92c2f9baad5d"
   # tag "linuxbrew"
 
-  option "with-check",      "Run a check before install (for all packages)"
-  option "with-static",     "Build static libraries (not recommended)"
-  option "with-docs",       "Build documentation (where applicable)"
+  option "with-docs",       "Build documentation and specifications (where applicable)"
   option "with-python3",    "Build xcb-proto with python3 (default python is used otherwise)"
+  option "with-check",      "Run a check before install (where applicable)"
+  option "with-static",     "Build static libraries (not recommended)"
 
-  depends_on "fontconfig"         => :build
-  depends_on "xorg-sgml-doctools" => [:build, :recommended]
+  # Build a list of requested compile flags for formulas
+  args_docs       = "#{ build.with?("docs")   ? "with-docs"        : "" }"
+  args_devel_docs = "#{ build.with?("docs")   ? "with-devel-docs"  : "" }"
+  args_specs      = "#{ build.with?("docs")   ? "with-specs"       : "" }"
+  args_check      = "#{ build.with?("check")  ? "with-check"       : "" }"
+  args_static     = "#{ build.with?("static") ? "with-static"      : "" }"
 
+  # Frequently Used Option
+  args_check_static = [args_check, args_static].reject(&:empty?)
+
+  # If python3 has been requested,
+  # add a direct dependence on xcb-proto package with python3 option
   if build.with?("python3")
-    depends_on :python3      => :build
-  else
-    depends_on :python       => :build
+    depends_on "xcb-proto" =>  ["with-python3", args_check].reject(&:empty?)
   end
 
-  args = %W[]
-  args << "with-check"  if build.with?("check")
-  args << "with-static" if build.with?("static")
+  depends_on "xtrans"        => [args_docs].reject(&:empty?)
+  depends_on "libx11"        => [args_check, args_static, args_specs].reject(&:empty?)
+  depends_on "libxext"       => [args_check, args_static, args_specs].reject(&:empty?)
+  depends_on "libfs"         => args_check_static
+  depends_on "libice"        => [args_check, args_static, args_docs, args_specs].reject(&:empty?)
+  depends_on "libsm"         => [args_check, args_static, args_docs].reject(&:empty?)
+  depends_on "libxscrnsaver" => args_check_static
+  depends_on "libxt"         => [args_check, args_static, args_specs].reject(&:empty?)
+  depends_on "libxmu"        => [args_check, args_static, args_docs].reject(&:empty?)
+  depends_on "libxpm"        => args_check_static
+  depends_on "libxaw"        => [args_check, args_static, args_specs].reject(&:empty?)
+  depends_on "libxfixes"     => args_check_static
+  depends_on "libxcomposite" => args_check_static
+  depends_on "libxrender"    => args_check_static
+  depends_on "libxcursor"    => args_check_static
+  depends_on "libxdamage"    => args_check_static
+  depends_on "libfontenc"    => args_check_static
+  depends_on "libxfont"      => [args_check, args_static, args_devel_docs].reject(&:empty?)
+  depends_on "libxft"        => args_check_static
+  depends_on "libxi"         => [args_check, args_static, args_docs, args_specs].reject(&:empty?)
+  depends_on "libxinerama"   => args_check_static
+  depends_on "libxrandr"     => args_check_static
+  depends_on "libxres"       => args_check_static
+  depends_on "libxtst"       => [args_check, args_static, args_specs].reject(&:empty?)
+  depends_on "libxv"         => args_check_static
+  depends_on "libxvmc"       => args_check_static
+  depends_on "libxxf86dga"   => args_check_static
+  depends_on "libxxf86vm"    => args_check_static
+  depends_on "libdmx"        => args_check_static
+  depends_on "libpciaccess"  => args_check_static
+  depends_on "libxkbfile"    => args_check_static
+  depends_on "libxshmfence"  => args_check_static
 
-  if build.with?("docs")
-    depends_on "xorg-docs"          => [:build, :recommended]
-    depends_on "fop"                => [:build, :recommended]
-    depends_on "libxslt"            => [:build, :recommended]
-    depends_on "xmlto"              => [:build, :recommended]
-    depends_on "asciidoc"           => [:build, :optional]
-  end
+  if build.with?("docs") # or build.with?("specs")
+    depends_on "xorg-docs" => :recommended
+    # xorg-docs is not a part of Xorg Libraries.
+    # We build it by default when documentation / specifications are requested
 
-  args << "without-xorg-sgml-doctools" if build.without?("xorg-sgml-doctools")
-  args << "with-fop"                   if build.with?("fop")
-  args << "with-libxsltproc"           if build.with?("libxslt")
-  args << "with-xmlto"                 if build.with?("xmlto")
-  args << "with-asciidoc"              if build.with?("asciidoc")
-
-  res = %w[xtrans libx11 libxext libfs libice libsm libxscrnsaver libxt libxmu libxpm libxaw libxfixes libxcomposite libxrender libxcursor libxdamage libfontenc libxfont libxft libxi libxinerama libxrandr libxres libxtst libxv libxvmc libxxf86dga libxxf86vm libdmx libpciaccess libxkbfile libxshmfence]
-
-  res.each do |r|
-    if r == "libxfont"
-      args2 = args
-      args2 << "--with-devel-docs" if build.with?("docs")
-      depends_on r => args2
-    elsif r == "xtrans"
-      args2 = args
-      args2 << "--with-docs" if build.with?("docs")
-      depends_on r => args2
-    elsif r == "xcb-proto"
-      args2 = args
-      args2 << "--with-python3" if build.with?("python3")
-      depends_on r => args2
-    else
-      depends_on r => args
-    end
+    ### Provide a way to override default options for the packages.
+    ### These options will be later combined into args_extra variable
+    ### and passed to formulas that accept them
+    # option "without-fop",     "Build without fop support"
+    # option "without-libxslt", "Build without libxslt support"
+    # option "without-xorg-sgml-doctools", "Build without xorg-sgml-doctools support"
+    # ###
   end
 
   def install
