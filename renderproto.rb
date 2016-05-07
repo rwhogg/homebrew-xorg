f9ae05d1aa1ba4cfe4b9ac1f656a85f2e4970deb
Bob W. Hogg <rwhogg@linux.com>
2016-05-06 21:23:34 -0400

    renderproto: add 0.11.1 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-xorg#39.
    Signed-off-by: Bob W. Hogg <rwhogg@linux.com>

--- renderproto.rb
+++ renderproto.rb
@@ -5,6 +5,11 @@ class Renderproto < Formula
   sha256 "06735a5b92b20759204e4751ecd6064a2ad8a6246bb65b3078b862a00def2537"
   # tag "linuxbrew"
 
+  bottle do
+    cellar :any
+    sha256 "212f70f35f2826cf764f65c116f9a0a6182a460adac7389deb6e74ea8cd7f54d" => :x86_64_linux
+  end
+
   depends_on "pkg-config"         =>  :build
   depends_on "util-macros"        =>  :build
 
