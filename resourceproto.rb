0cf2b3c4d337b609b2a4255710be4b921def1270
Bob W. Hogg <rwhogg@linux.com>
2016-05-06 21:28:13 -0400

    resourceproto: add 1.2.0 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-xorg#40.
    Signed-off-by: Bob W. Hogg <rwhogg@linux.com>

--- resourceproto.rb
+++ resourceproto.rb
@@ -5,6 +5,11 @@ class Resourceproto < Formula
   sha256 "3c66003a6bdeb0f70932a9ed3cf57cc554234154378d301e0c5cfa189d8f6818"
   # tag "linuxbrew"
 
+  bottle do
+    cellar :any
+    sha256 "86232aafd92fda6c30e16b16cf03b17df6178495d13ce31baf47f4ec381e2630" => :x86_64_linux
+  end
+
   depends_on "pkg-config"         =>  :build
   depends_on "util-macros"        =>  :build
 
