c25f1c77f88bb6e245a850b2d3ceaaa1095d1c3b
Bob W. Hogg <rwhogg@linux.com>
2016-05-07 08:24:36 -0400

    kbproto: add 1.0.7 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-xorg#43.
    Signed-off-by: Bob W. Hogg <rwhogg@linux.com>

--- kbproto.rb
+++ kbproto.rb
@@ -5,6 +5,11 @@ class Kbproto < Formula
   sha256 "f882210b76376e3fa006b11dbd890e56ec0942bc56e65d1249ff4af86f90b857"
   # tag "linuxbrew"
 
+  bottle do
+    cellar :any
+    sha256 "3d6b25f6545716ad960db544ddd46b60c5e73c46fcc4a8b6ebea34e8fcdaf478" => :x86_64_linux
+  end
+
   option "with-specs",  "Build specifications"
 
   depends_on "pkg-config"         =>  :build
