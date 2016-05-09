b953bb275df236d78702b39a6a6e337c0ad6967c
Bob W. Hogg <rwhogg@linux.com>
2016-05-08 21:37:41 -0400

    xcb-util: add 0.4.0 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-xorg#65.
    Signed-off-by: Bob W. Hogg <rwhogg@linux.com>

--- xcb-util.rb
+++ xcb-util.rb
@@ -4,6 +4,11 @@ class XcbUtil < Formula
   url "http://xcb.freedesktop.org/dist/xcb-util-0.4.0.tar.bz2"
   sha256 "46e49469cb3b594af1d33176cd7565def2be3fa8be4371d62271fabb5eae50e9"
 
+  bottle do
+    cellar :any
+    sha256 "c9b90cb2c9fa611e3b5d31a52d544fd6a173fc33cfd6f057f876ab7112bac179" => :x86_64_linux
+  end
+
   option "with-static", "Build static libraries (not recommended)"
 
   depends_on "pkg-config" => :build
