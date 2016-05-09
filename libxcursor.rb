e8c4a0f56618f81c27771f831226370d1704d2a6
Bob W. Hogg <rwhogg@linux.com>
2016-05-08 22:03:50 -0400

    libxcursor: add 1.1.14 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-xorg#72.
    Signed-off-by: Bob W. Hogg <rwhogg@linux.com>

--- libxcursor.rb
+++ libxcursor.rb
@@ -5,6 +5,10 @@ class Libxcursor < Formula
   sha256 "9bc6acb21ca14da51bda5bc912c8955bc6e5e433f0ab00c5e8bef842596c33df"
   # tag "linuxbrew"
 
+  bottle do
+    sha256 "b416d2de1edef471cebc301b9865ae1ccd79480a6bae60685b637b2ca65f308a" => :x86_64_linux
+  end
+
   option "without-test", "Skip compile-time tests"
   option "with-static", "Build static libraries (not recommended)"
 
