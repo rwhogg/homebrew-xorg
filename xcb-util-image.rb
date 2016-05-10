58e4fe08474e055b78b8d1bcb6c73bb94cafa383
Maxim Belkin <maxim.belkin@gmail.com>
2016-05-10 01:09:47 -0500

    xcb-util-image: without-test option and better configure flags

--- xcb-util-image.rb
+++ xcb-util-image.rb
@@ -4,6 +4,7 @@ class XcbUtilImage < Formula
   url "http://xcb.freedesktop.org/dist/xcb-util-image-0.4.0.tar.bz2"
   sha256 "2db96a37d78831d643538dd1b595d7d712e04bdccf8896a5e18ce0f398ea2ffc"
 
+  option "without-test",    "Skip compile-time tests"
   option "with-static", "Build static libraries (not recommended)"
 
   depends_on "pkg-config" => :build
@@ -18,7 +19,9 @@ class XcbUtilImage < Formula
       --disable-dependency-tracking
       --disable-silent-rules
     ]
-    args << "--disable-static" if !build.with?("static")
+
+    # Be explicit about the configure flags
+    args << "--enable-static=#{build.with?("static") ? "yes" : "no"}"
 
     system "./configure", *args
     system "make"
