617839f5e10e9125d74752fd241a1173c0203bd6
Bob W. Hogg <rwhogg@linux.com>
2016-05-09 20:42:35 -0400

    libxft: add 2.3.2 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-xorg#75.
    Signed-off-by: Bob W. Hogg <rwhogg@linux.com>

--- libxft.rb
+++ libxft.rb
@@ -5,6 +5,11 @@ class Libxft < Formula
   sha256 "f5a3c824761df351ca91827ac221090943ef28b248573486050de89f4bfcdc4c"
   # tag "linuxbrew"
 
+  bottle do
+    cellar :any
+    sha256 "1c214681d1f4ec2feacdf2bd6bd24d9e51b22d24aa2b191c2eda87d85aba0f82" => :x86_64_linux
+  end
+
   option "without-test", "Skip compile-time tests"
   option "with-static", "Build static libraries (not recommended)"
   option "with-brewed-bzip2", "Use brewed bzip2"
