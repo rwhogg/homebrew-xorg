c1f1f8b89e79c952dd5c4a348a6a4fb5ab2590e7
Maxim Belkin <maxim.belkin@gmail.com>
2016-05-10 01:39:26 -0500

    xcb-util-image: new options (without-test, with-docs), util-macros and doxygen deps, flags, compile-time test

--- xcb-util-image.rb
+++ xcb-util-image.rb
@@ -4,10 +4,13 @@ class XcbUtilImage < Formula
   url "http://xcb.freedesktop.org/dist/xcb-util-image-0.4.0.tar.bz2"
   sha256 "2db96a37d78831d643538dd1b595d7d712e04bdccf8896a5e18ce0f398ea2ffc"
 
-  option "without-test",    "Skip compile-time tests"
-  option "with-static", "Build static libraries (not recommended)"
+  option "without-test", "Skip compile-time tests"
+  option "with-static",  "Build static libraries (not recommended)"
+  option "with-docs",    "Regenerate documentation (requires doxygen)"
 
   depends_on "pkg-config" => :build
+  depends_on "util-macros" => :build
+  depends_on "doxygen" => :build if build.with?("docs")
   depends_on "libxcb"
   depends_on "xcb-util"
 
@@ -22,9 +25,16 @@ class XcbUtilImage < Formula
 
     # Be explicit about the configure flags
     args << "--enable-static=#{build.with?("static") ? "yes" : "no"}"
+    args << "--enable-devel-docs=#{build.with?("docs") ? "yes" : "no"}"
+    args << "--with-doxygen=#{build.with?("docs") ? "yes" : "no"}"
+
 
     system "./configure", *args
     system "make"
+    if build.with?("test")
+      ENV["LD_LIBRARY_PATH"] = "#{lib}"
+      system "make", "check"
+    end
     system "make", "install"
   end
 end
