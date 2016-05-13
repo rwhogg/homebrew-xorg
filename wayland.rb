28323d7c71348ed77e09097d0332949726150f94
Maxim Belkin <maxim.belkin@gmail.com>
2016-05-13 03:25:10 -0500

    wayland: new formula

---
+++ wayland.rb
@@ -0,0 +1,41 @@
+class Wayland < Formula
+  desc "A protocol for a compositor to talk to its clients"
+  homepage "https://wayland.freedesktop.org"
+  url "http://wayland.freedesktop.org/releases/wayland-1.10.0.tar.xz"
+  sha256 "4bf6e790aa6f50ab3825676282ecd75850ec9c4767af96ecb7127b1f3c3d60dc"
+
+  head do
+    url "git://anongit.freedesktop.org/wayland/wayland"
+  end
+
+  option "with-static", "Build static libraries (not recommended)"
+  option "without-test", "Skip compile-time tests"
+
+  depends_on "pkg-config"  => :build
+  depends_on "expat"
+  depends_on "libffi"
+  depends_on "autoconf" if build.head?
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --sysconfdir=#{etc}
+      --localstatedir=#{var}
+      --disable-dependency-tracking
+      --disable-silent-rules
+      --disable-documentation
+    ]
+
+    args << "--enable-static=#{build.with?("static") ? "yes" : "no"}"
+
+    if build.head?
+      system "./autogen.sh", *args
+    else
+      system "./configure", *args
+    end
+    system "make"
+    ENV.deparallelize
+    system "make", "check" if build.with?("test")
+    system "make", "install"
+  end
+end
