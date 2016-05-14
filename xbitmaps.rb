fb6c7395d05a5aae64960645afcc30f0f74d9d85
Bob W. Hogg <rwhogg@linux.com>
2016-05-13 22:42:11 -0400

    xbitmaps: add 1.1.1 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-xorg#91.
    Signed-off-by: Bob W. Hogg <rwhogg@linux.com>

--- xbitmaps.rb
+++ xbitmaps.rb
@@ -4,6 +4,11 @@ class Xbitmaps < Formula
   url "http://xorg.freedesktop.org/archive/individual/data/xbitmaps-1.1.1.tar.bz2"
   sha256 "3671b034356bbc4d32d052808cf646c940ec8b2d1913adac51b1453e41aa1e9d"
 
+  bottle do
+    cellar :any
+    sha256 "fb234ad593ff26c68e520b1c33ad27c6668b2cd1c6a569c08f9e6608ce74d9b1" => :x86_64_linux
+  end
+
   depends_on "pkg-config"  => :build
   depends_on "util-macros" => :build
 
