1055e2bfc4cfb38a078f4966c58d8a80d1a6a9dc
Maxim Belkin <maxim.belkin@gmail.com>
2016-05-18 05:56:46 -0500

    mkfontdir and mkfontscale: new formulas

---
+++ mkfontdir.rb
@@ -0,0 +1,27 @@
+class Mkfontdir < Formula
+  desc "X.Org Applications: mkfontdir"
+  homepage "http://www.x.org/"
+  ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7app.html
+  url    "https://www.x.org/pub/individual/app/mkfontdir-1.0.7.tar.bz2"
+  mirror "http://ftp.x.org/pub/individual/app/mkfontdir-1.0.7.tar.bz2"
+  sha256 "56d52a482df130484e51fd066d1b6eda7c2c02ddbc91fe6e2be1b9c4e7306530"
+  # tag "linuxbrew"
+
+
+  depends_on "pkg-config"  => :build
+  depends_on "util-macros" => :build
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --sysconfdir=#{etc}
+      --localstatedir=#{var}
+      --disable-dependency-tracking
+      --disable-silent-rules
+    ]
+
+    system "./configure", *args
+    system "make"
+    system "make", "install"
+  end
+end
---
+++ mkfontscale.rb
@@ -0,0 +1,31 @@
+class Mkfontscale < Formula
+  desc "X.Org Applications: mkfontscale"
+  homepage "http://www.x.org/"
+  ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7app.html
+  url    "https://www.x.org/pub/individual/app/mkfontscale-1.1.2.tar.bz2"
+  mirror "http://ftp.x.org/pub/individual/app/mkfontscale-1.1.2.tar.bz2"
+  sha256 "8c6d5228af885477b9aec60ca6f172578e7d2de42234357af62fb00439453f20"
+  # tag "linuxbrew"
+
+  option "with-bzip2", "Support bzip2 compressed bitmap fonts"
+
+  depends_on "pkg-config" =>  :build
+  depends_on "libfontenc"
+  depends_on "freetype"
+  depends_on "bzip2" if build.with?("bzip2")
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --sysconfdir=#{etc}
+      --localstatedir=#{var}
+      --disable-dependency-tracking
+      --disable-silent-rules
+    ]
+    args << "--with-bzip2" if build.with?("bzip2")
+
+    system "./configure", *args
+    system "make"
+    system "make", "install"
+  end
+end
