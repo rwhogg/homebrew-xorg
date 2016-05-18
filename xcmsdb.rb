c5f09269944673999ea1a03e73218051a1c4aefb
Maxim Belkin <maxim.belkin@gmail.com>
2016-05-18 12:55:42 -0500

    xcursorgen: new formula

---
+++ xcmsdb.rb
@@ -0,0 +1,27 @@
+class Xcmsdb < Formula
+  desc "X.Org Applications: xcmsdb"
+  homepage "http://www.x.org/"
+  ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7app.html
+  url    "https://www.x.org/pub/individual/app/xcmsdb-1.0.5.tar.bz2"
+  mirror "http://ftp.x.org/pub/individual/app/xcmsdb-1.0.5.tar.bz2"
+  sha256 "e5585361bb8b6a05bb814a8d0e444ee93e0f00180881d3070aff4571e97f67c6"
+  # tag "linuxbrew"
+
+
+  depends_on "pkg-config" =>  :build
+  depends_on "libx11"
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --sysconfdir=#{etc}
+      --localstatedir=#{var}
+      --disable-dependency-tracking
+      --disable-silent-rules
+    ]
+
+    system "./configure", *args
+    system "make"
+    system "make", "install"
+  end
+end
---
+++ xcursorgen.rb
@@ -0,0 +1,29 @@
+class Xcursorgen < Formula
+  desc "X.Org Applications: xcursorgen"
+  homepage "http://www.x.org/"
+  ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7app.html
+  url    "https://www.x.org/pub/individual/app/xcursorgen-1.0.6.tar.bz2"
+  mirror "http://ftp.x.org/pub/individual/app/xcursorgen-1.0.6.tar.bz2"
+  sha256 "31c8910f54eb175a8a74a60e7662697467e21a8bf948220a6048a93924b3f66c"
+  # tag "linuxbrew"
+
+
+  depends_on "pkg-config" =>  :build
+  depends_on "libx11"
+  depends_on "libxcursor"
+  depends_on "libpng"
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --sysconfdir=#{etc}
+      --localstatedir=#{var}
+      --disable-dependency-tracking
+      --disable-silent-rules
+    ]
+
+    system "./configure", *args
+    system "make"
+    system "make", "install"
+  end
+end
