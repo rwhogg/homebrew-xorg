2f42530a918222e65fe0f58e034f7bf0aa7fecaa
LinuxbrewTestBot <testbot@linuxbrew.sh>
2016-05-19 16:58:36 +0000

    libxshmfence: add 1.2 bottle for Linuxbrew.

--- libxshmfence.rb
+++ libxshmfence.rb
@@ -5,6 +5,11 @@ class Libxshmfence < Formula
   sha256 "d21b2d1fd78c1efbe1f2c16dae1cb23f8fd231dcf891465b8debe636a9054b0c"
   # tag "linuxbrew"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "5a9e167ece906a18ac6503d6b3f61c8b64f6b9749c1290079f31e8d9e712cf9d" => :x86_64_linux
+  end
+
   option "without-test", "Skip compile-time tests"
   option "with-static", "Build static libraries (not recommended)"
 
