e1b38e137caf5160ff17a50294a70af7a69b34ed
Maxim Belkin <maxim.belkin@gmail.com>
2016-05-22 05:52:33 -0500

    libfontenc.rb: added missing dependency on encodings package

--- libfontenc.rb
+++ libfontenc.rb
@@ -15,6 +15,7 @@ class Libfontenc < Formula
   depends_on "pkg-config" =>  :build
   depends_on "xproto"     =>  :build
   depends_on "font-util"  =>  :build
+  depends_on "encodings"  =>  :build
   depends_on "zlib"
 
   def install
