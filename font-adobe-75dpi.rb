47982c07ca6d7f275947f6a749de6e39895721ca
Bob W. Hogg <rwhogg@linux.com>
2016-05-25 21:34:04 -0400

    font-adobe-75dpi: add 1.0.3 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-xorg#103.
    Signed-off-by: Bob W. Hogg <rwhogg@linux.com>

--- font-adobe-75dpi.rb
+++ font-adobe-75dpi.rb
@@ -8,6 +8,10 @@ class FontAdobe75dpi < Formula
   sha256 "c6024a1e4a1e65f413f994dd08b734efd393ce0a502eb465deb77b9a36db4d09"
   # tag "linuxbrew"
 
+  bottle do
+    sha256 "7b425c35d4f255a4fa59a32c24fbefae73953cf45b7940b3b4cc985a132c384d" => :x86_64_linux
+  end
+
   depends_on "pkg-config" =>  :build
   depends_on "font-util"  =>  :build
   depends_on "bdftopcf"   =>  :build
