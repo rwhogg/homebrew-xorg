c06fcde4c2e269eedf6a277ae92b26a8c4e8df73
Bob W. Hogg <rwhogg@linux.com>
2016-05-29 20:07:07 -0400

    font-cursor-misc: add 1.0.3 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-xorg#117.
    Signed-off-by: Bob W. Hogg <rwhogg@linux.com>

--- font-cursor-misc.rb
+++ font-cursor-misc.rb
@@ -8,6 +8,10 @@ class FontCursorMisc < Formula
   sha256 "17363eb35eece2e08144da5f060c70103b59d0972b4f4d77fd84c9a7a2dba635"
   # tag "linuxbrew"
 
+  bottle do
+    sha256 "bb1d37bcc3afb9c16410df0b76e276582cbbd9090705e152c25f6d4662001362" => :x86_64_linux
+  end
+
   depends_on "pkg-config" =>  :build
   depends_on "font-util"  =>  :build
   depends_on "bdftopcf"   =>  :build
