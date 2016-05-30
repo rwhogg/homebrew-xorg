ba269bbf5473ed4f611ce04c4cb4baca11cc5f17
Bob W. Hogg <rwhogg@linux.com>
2016-05-30 11:39:50 -0400

    font-sony-misc: add 1.0.3 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-xorg#131.
    Signed-off-by: Bob W. Hogg <rwhogg@linux.com>

--- font-sony-misc.rb
+++ font-sony-misc.rb
@@ -8,6 +8,10 @@ class FontSonyMisc < Formula
   sha256 "2043a326ba347c9da5ca1e9bc363e2521c3ea40b43b1f9662d333efd4867cff5"
   # tag "linuxbrew"
 
+  bottle do
+    sha256 "c77dc9ca04118b63a147cf8c8fb582f661810e3030cf53e17b28ce1467cf428a" => :x86_64_linux
+  end
+
   depends_on "pkg-config" =>  :build
   depends_on "font-util"  =>  :build
   depends_on "bdftopcf"   =>  :build
