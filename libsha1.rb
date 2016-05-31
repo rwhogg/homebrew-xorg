33f5d10c47a3323eaf40105b25aac7f1abc1c81f
LinuxbrewTestBot <testbot@linuxbrew.sh>
2016-05-31 12:58:27 +0000

    libsha1: add 0.3 bottle for Linuxbrew.

--- libsha1.rb
+++ libsha1.rb
@@ -4,6 +4,11 @@ class Libsha1 < Formula
   url "https://github.com/dottedmag/libsha1.git"
   version "0.3"
 
+  bottle do
+    cellar :any
+    sha256 "3ff1d504fc8fd266bf7d7e4c67c2afd0bb56e8e2525507d9e2a69a6e76e8c714" => :x86_64_linux
+  end
+
   option "with-static", "Build static libraries (not recommended)"
 
   depends_on "autoconf" => :build
