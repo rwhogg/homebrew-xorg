7402118b49b039f99349f72752b6f9a92c3d7fe7
Bob W. Hogg <rwhogg@linux.com>
2016-07-25 13:45:34 -0400

    xprop: add 1.2.2 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-xorg#157.
    Signed-off-by: Bob W. Hogg <rwhogg@linux.com>

--- xprop.rb
+++ xprop.rb
@@ -7,6 +7,11 @@ class Xprop < Formula
   sha256 "9bee88b1025865ad121f72d32576dd3027af1446774aa8300cce3c261d869bc6"
   # tag "linuxbrew"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "88570f896f5181fc75405867ecdabc6d901b5d21dfe54c619430d9b5cb521ec1" => :x86_64_linux
+  end
+
   depends_on "pkg-config" =>  :build
   depends_on "xproto" => :build
   depends_on "libx11"
