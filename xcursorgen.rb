221b4bff6fd8da9ee9a106143fdc2e1827a92f48
Bob W. Hogg <rwhogg@linux.com>
2016-07-25 14:09:53 -0400

    xcursorgen: add 1.0.6 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-xorg#153.
    Signed-off-by: Bob W. Hogg <rwhogg@linux.com>

--- xcursorgen.rb
+++ xcursorgen.rb
@@ -7,6 +7,10 @@ class Xcursorgen < Formula
   sha256 "31c8910f54eb175a8a74a60e7662697467e21a8bf948220a6048a93924b3f66c"
   # tag "linuxbrew"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "5eb5cc0e64c2c05bf5c8f626e0aa8f796bf8d1a572d1d6bff9916f6bae6a1510" => :x86_64_linux
+  end
 
   depends_on "pkg-config" =>  :build
   depends_on "libx11"
