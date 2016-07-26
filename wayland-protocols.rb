6916587a00666cc9dd7e7cd36b080d0da4b2250e
Maxim Belkin <maxim.belkin@gmail.com>
2016-07-26 18:35:53 -0500

    wayland-protocols: new formula

---
+++ wayland-protocols.rb
@@ -0,0 +1,32 @@
+class WaylandProtocols < Formula
+  desc "A protocol for a compositor to talk to its clients"
+  homepage "https://wayland.freedesktop.org"
+  url "https://wayland.freedesktop.org/releases/wayland-protocols-1.5.tar.xz"
+  sha256 "7d0b426ccaeafc084fc579bb2a139f8ad4ea3492d54b7b0210d124c61fbeb2bf"
+
+  head do
+    url "git://anongit.freedesktop.org/wayland/wayland-protocols"
+  end
+
+  option "without-test", "Skip compile-time tests"
+
+  depends_on "pkg-config"  => :build
+  depends_on "wayland"
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --sysconfdir=#{etc}
+      --localstatedir=#{var}
+      --disable-dependency-tracking
+      --disable-silent-rules
+    ]
+
+    args << "--enable-static=#{build.with?("static") ? "yes" : "no"}"
+
+    system "./configure", *args
+    system "make"
+    system "make", "check" if build.with?("test")
+    system "make", "install"
+  end
+end
