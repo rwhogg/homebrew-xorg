bf19f37a01acbb4aea8df9bd2778d842b60e1f92
Maxim Belkin <maxim.belkin@gmail.com>
2016-07-28 12:06:27 -0500

    mtdev: add 1.1.5 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-xorg#173.
    Signed-off-by: Maxim Belkin <maxim.belkin@gmail.com>

--- mtdev.rb
+++ mtdev.rb
@@ -1,6 +1,11 @@
 class Mtdev < Formula
   desc "Multitouch Protocol Translation Library"
   homepage "http://bitmath.org"
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "32778f8b518f949bd06e4ec20c3d9d0915febaedfb01011c7ab936076d88cd51" => :x86_64_linux
+  end
+
   url    "http://bitmath.org/code/mtdev/mtdev-1.1.5.tar.bz2"
   sha256 "6677d5708a7948840de734d8b4675d5980d4561171c5a8e89e54adf7a13eba7f"
   # tag "linuxbrew"
