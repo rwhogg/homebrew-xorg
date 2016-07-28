1eb9083831647ac03c8da967b07c4e0f732740d5
Maxim Belkin <maxim-belkin@users.noreply.github.com>
2016-07-28 11:46:18 -0500

    mtdev: new formula (#171)
    mtdev: new formula
    
    Closes Linuxbrew/homebrew-xorg#171
    Signed-off-by: Maxim Belkin <maxim.belkin@gmail.com>

---
+++ mtdev.rb
@@ -0,0 +1,32 @@
+class Mtdev < Formula
+  desc "Multitouch Protocol Translation Library"
+  homepage "http://bitmath.org"
+  url    "http://bitmath.org/code/mtdev/mtdev-1.1.5.tar.bz2"
+  sha256 "6677d5708a7948840de734d8b4675d5980d4561171c5a8e89e54adf7a13eba7f"
+  # tag "linuxbrew"
+
+  option "with-static", "Build static libraries (not recommended)"
+  depends_on "pkg-config" =>  :build
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --sysconfdir=#{etc}
+      --localstatedir=#{var}
+      --disable-dependency-tracking
+      --disable-silent-rules
+    ]
+
+    # Be explicit about the configure flags
+    args << "--enable-static=#{build.with?("static") ? "yes" : "no"}"
+
+    system "./configure", *args
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    output = shell_output("ldd #{bin}/mtdev-test").chomp
+    assert_match "lib/libmtdev.so.1", output
+  end
+end
