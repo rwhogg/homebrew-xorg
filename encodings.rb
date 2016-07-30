40f4f363e2433e424248c4c938fd5d8a95dae7e1
Maxim Belkin <maxim.belkin@gmail.com>
2016-07-29 22:23:33 -0500

    encodings: add 1.0.4 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-xorg#184.
    Signed-off-by: Maxim Belkin <maxim.belkin@gmail.com>

--- encodings.rb
+++ encodings.rb
@@ -8,6 +8,11 @@ class Encodings < Formula
   sha256 "ced6312988a45d23812c2ac708b4595f63fd7a49c4dcd9f66bdcd50d1057d539"
   # tag "linuxbrew"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "08ecde6fe4edbf50e67b7c3464f7b55e51d031e73f81ef344f5b38717725043c" => :x86_64_linux
+  end
+
   depends_on "pkg-config" =>  :build
   depends_on "font-util"  =>  :build
   depends_on "mkfontscale" => :build
