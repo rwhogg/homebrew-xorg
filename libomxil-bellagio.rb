9fe291630c35b49af3c2fbe600003795006ac809
LinuxbrewTestBot <testbot@linuxbrew.sh>
2016-09-02 23:40:56 +0000

    libomxil-bellagio: add 0.9.3 bottle for Linuxbrew.

--- libomxil-bellagio.rb
+++ libomxil-bellagio.rb
@@ -4,6 +4,10 @@ class LibomxilBellagio < Formula
   url "https://downloads.sourceforge.net/project/omxil/omxil/Bellagio%200.9.3/libomxil-bellagio-0.9.3.tar.gz"
   sha256 "593c0729c8ef8c1467b3bfefcf355ec19a46dd92e31bfc280e17d96b0934d74c"
 
+  bottle do
+    sha256 "f9033e9fd118666ba8cb7cfb540cb62db69613e369e48a0c18b32c92b9b3aab0" => :x86_64_linux
+  end
+
   option "without-test", "Skip compile-time tests"
   option "with-static",   "Build static libraries (not recommended)"
 
