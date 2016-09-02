daaf519f7432f4ce9e5a19671efa0da5277a9c3e
Bob W. Hogg <rwhogg@linux.com>
2016-09-02 19:10:59 -0400

    xlsclients: add 1.1.3 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-xorg#165.
    Signed-off-by: Bob W. Hogg <rwhogg@linux.com>

--- xlsclients.rb
+++ xlsclients.rb
@@ -7,6 +7,10 @@ class Xlsclients < Formula
   sha256 "5d9666fcc6c3de210fc70d5a841a404955af709a616fde530fe4e8f7723e3d3d"
   # tag "linuxbrew"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "5d7dbf8acf9f395456e20cd8b82fb4d118a686924da4311e07e46606d541417d" => :x86_64_linux
+  end
 
   depends_on "pkg-config" =>  :build
   depends_on "libxcb"
