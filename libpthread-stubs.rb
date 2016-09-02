9ddf0629fe3f5dc7dc436a725f214da45f5aec27
Bob W. Hogg <rwhogg@linux.com>
2016-09-02 19:09:08 -0400

    libpthread-stubs: add 0.3 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-xorg#201.
    Signed-off-by: Bob W. Hogg <rwhogg@linux.com>

--- libpthread-stubs.rb
+++ libpthread-stubs.rb
@@ -5,6 +5,11 @@ class LibpthreadStubs < Formula
   sha256 "35b6d54e3cc6f3ba28061da81af64b9a92b7b757319098172488a660e3d87299"
   # tag "linuxbrew"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "ab61c92ab2a15907df6a29d08963290c4896b1aa7939524932d343acbe7d8026" => :x86_64_linux
+  end
+
   depends_on "pkg-config"  => :build
 
   def install
