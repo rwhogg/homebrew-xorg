22c6bc6a644d035d9b0d3f209fe263254ff660eb
Bob W. Hogg <rwhogg@linux.com>
2016-09-13 17:20:28 -0400

    xcb-util-wm: audit fixes (#215)

--- xcb-util-wm.rb
+++ xcb-util-wm.rb
@@ -1,7 +1,7 @@
 class XcbUtilWm < Formula
-  desc "Libraries which provide client and window-manager helpers for EWMH and ICCCM"
+  desc "Client and window-manager helpers for EWMH and ICCCM"
   homepage "http://xcb.freedesktop.org"
-  url "http://xcb.freedesktop.org/dist/xcb-util-wm-0.4.1.tar.bz2"
+  url "https://xcb.freedesktop.org/dist/xcb-util-wm-0.4.1.tar.bz2"
   sha256 "28bf8179640eaa89276d2b0f1ce4285103d136be6c98262b6151aaee1d3c2a3f"
 
   bottle do
