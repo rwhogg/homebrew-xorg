99464bc41f93a864eb327c122dcb123a5de051a9
Bob W. Hogg <rwhogg@linux.com>
2016-09-13 16:47:37 -0400

    xcb-util-wm: add 0.4.1 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-xorg#214.
    Signed-off-by: Bob W. Hogg <rwhogg@linux.com>

--- xcb-util-wm.rb
+++ xcb-util-wm.rb
@@ -4,6 +4,11 @@ class XcbUtilWm < Formula
   url "http://xcb.freedesktop.org/dist/xcb-util-wm-0.4.1.tar.bz2"
   sha256 "28bf8179640eaa89276d2b0f1ce4285103d136be6c98262b6151aaee1d3c2a3f"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "86fd71256a1ba514acfb1f4018bdb797b8035d45427305d2a6e4f14576fa8e96" => :x86_64_linux
+  end
+
   option "without-test", "Skip compile-time tests"
   option "with-static",  "Build static libraries (not recommended)"
   option "with-docs",    "Regenerate documentation (requires doxygen)"
