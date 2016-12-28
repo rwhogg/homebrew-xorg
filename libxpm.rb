5bf6924756969dd424eaf259dde1e21ee99beeac
Bob W. Hogg <rwhogg@linux.com>
2016-12-28 08:28:23 -0800

    libxpm 3.5.12
    Closes Linuxbrew/homebrew-xorg#243.
    
    Signed-off-by: Bob W. Hogg <rwhogg@linux.com>

--- libxpm.rb
+++ libxpm.rb
@@ -1,8 +1,8 @@
 class Libxpm < Formula
   desc "X.Org Libraries: libXpm"
   homepage "http://www.x.org/" ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7lib.html
-  url "http://ftp.x.org/pub/individual/lib/libXpm-3.5.11.tar.bz2"
-  sha256 "c5bdafa51d1ae30086fac01ab83be8d47fe117b238d3437f8e965434090e041c"
+  url "http://ftp.x.org/pub/individual/lib/libXpm-3.5.12.tar.bz2"
+  sha256 "fd6a6de3da48de8d1bb738ab6be4ad67f7cb0986c39bd3f7d51dd24f7854bdec"
   # tag "linuxbrew"
 
   bottle do
