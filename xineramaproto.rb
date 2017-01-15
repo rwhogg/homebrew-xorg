8cef4ab7f49e39d982512140b2eb6143e8b35095
Bob W. Hogg <rwhogg@linux.com>
2017-01-15 10:58:32 -0800

    xineramaproto: use https URL

--- xineramaproto.rb
+++ xineramaproto.rb
@@ -1,7 +1,7 @@
 class Xineramaproto < Formula
   desc "X.Org Protocol Headers: xineramaproto"
   homepage "http://www.x.org/" ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7lib.html
-  url    "http://ftp.x.org/pub/individual/proto/xineramaproto-1.2.1.tar.bz2"
+  url "https://www.x.org/archive/individual/proto/xineramaproto-1.2.1.tar.bz2"
   sha256 "977574bb3dc192ecd9c55f59f991ec1dff340be3e31392c95deff423da52485b"
   # tag "linuxbrew"
 
@@ -10,8 +10,8 @@ class Xineramaproto < Formula
     sha256 "42b4c1562aa8cfa5fc8ed49a2226bac13eb01c1b25d00e147870b342dea99ff2" => :x86_64_linux
   end
 
-  depends_on "pkg-config"         =>  :build
-  depends_on "util-macros"        =>  :build
+  depends_on "pkg-config" => :build
+  depends_on "util-macros" => :build
 
   def install
     args = %W[
