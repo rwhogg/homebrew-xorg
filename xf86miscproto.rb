0767ec3cb08056e2c0532628af143ad45b84e826
Bob W. Hogg <rwhogg@linux.com>
2017-01-15 10:58:32 -0800

    xf86miscproto: use https URL

--- xf86miscproto.rb
+++ xf86miscproto.rb
@@ -1,11 +1,11 @@
 class Xf86miscproto < Formula
   desc "X.Org Protocol Headers: xf86miscproto"
   homepage "http://www.x.org/" ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7lib.html
-  url    "https://www.x.org/archive//individual/proto/xf86miscproto-0.9.3.tar.bz2"
+  url    "https://www.x.org/archive/individual/proto/xf86miscproto-0.9.3.tar.bz2"
   sha256 "45b8ec6a4a8ca21066dce117e09dcc88539862e616e60fb391de05b36f63b095"
   # tag "linuxbrew"
 
-  depends_on "pkg-config"         =>  :build
+  depends_on "pkg-config" => :build
 
   def install
     args = %W[
