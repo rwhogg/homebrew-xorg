1b9af1744a49944b750acb063b5aa6762945f66c
Bob W. Hogg <rwhogg@linux.com>
2017-01-15 10:58:32 -0800

    xf86dgaproto: use https URL

--- xf86dgaproto.rb
+++ xf86dgaproto.rb
@@ -1,7 +1,7 @@
 class Xf86dgaproto < Formula
   desc "X.Org Protocol Headers: xf86dgaproto"
   homepage "http://www.x.org/" ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7lib.html
-  url    "http://ftp.x.org/pub/individual/proto/xf86dgaproto-2.1.tar.bz2"
+  url "https://www.x.org/archive/individual/proto/xf86dgaproto-2.1.tar.bz2"
   sha256 "ac5ef65108e1f2146286e53080975683dae49fc94680042e04bd1e2010e99050"
   # tag "linuxbrew"
 
@@ -10,8 +10,8 @@ class Xf86dgaproto < Formula
     sha256 "99c0abed1c8a54c5cf3e8a6048c9116f0f5450ee56b13cd617fb04569f8b8613" => :x86_64_linux
   end
 
-  depends_on "pkg-config"         =>  :build
-  depends_on "util-macros"        =>  :build
+  depends_on "pkg-config" => :build
+  depends_on "util-macros" => :build
 
   def install
     args = %W[
