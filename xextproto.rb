fc70fc7e1708cfe0cd54347f1540b0d171c6bef5
Bob W. Hogg <rwhogg@linux.com>
2017-01-15 10:58:32 -0800

    xextproto: use https URL

--- xextproto.rb
+++ xextproto.rb
@@ -1,7 +1,7 @@
 class Xextproto < Formula
   desc "X.Org Protocol Headers: xextproto"
   homepage "http://www.x.org/" ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7lib.html
-  url    "http://ftp.x.org/pub/individual/proto/xextproto-7.3.0.tar.bz2"
+  url "https://www.x.org/archive/individual/proto/xextproto-7.3.0.tar.bz2"
   sha256 "f3f4b23ac8db9c3a9e0d8edb591713f3d70ef9c3b175970dd8823dfc92aa5bb0"
   # tag "linuxbrew"
 
