749f07ff03beb969a0aaafcdae2191966fc157f9
Bob W. Hogg <rwhogg@linux.com>
2017-01-15 10:58:32 -0800

    resourceproto: use https URL

--- resourceproto.rb
+++ resourceproto.rb
@@ -1,7 +1,7 @@
 class Resourceproto < Formula
   desc "X.Org Protocol Headers: resourceproto"
   homepage "http://www.x.org/" ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7lib.html
-  url    "http://ftp.x.org/pub/individual/proto/resourceproto-1.2.0.tar.bz2"
+  url "https://www.x.org/archive/individual/proto/resourceproto-1.2.0.tar.bz2"
   sha256 "3c66003a6bdeb0f70932a9ed3cf57cc554234154378d301e0c5cfa189d8f6818"
   # tag "linuxbrew"
 
@@ -10,8 +10,8 @@ class Resourceproto < Formula
     sha256 "86232aafd92fda6c30e16b16cf03b17df6178495d13ce31baf47f4ec381e2630" => :x86_64_linux
   end
 
-  depends_on "pkg-config"         =>  :build
-  depends_on "util-macros"        =>  :build
+  depends_on "pkg-config" => :build
+  depends_on "util-macros" => :build
 
   def install
     args = %W[
