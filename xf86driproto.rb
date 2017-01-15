fd65c637f94697f114ecb0fcf8788d5bd34572d1
Bob W. Hogg <rwhogg@linux.com>
2017-01-15 10:58:32 -0800

    xf86driproto: use https URL

--- xf86driproto.rb
+++ xf86driproto.rb
@@ -1,7 +1,7 @@
 class Xf86driproto < Formula
   desc "X.Org Protocol Headers: xf86driproto"
   homepage "http://www.x.org/" ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7lib.html
-  url    "http://ftp.x.org/pub/individual/proto/xf86driproto-2.1.1.tar.bz2"
+  url "https://www.x.org/archive/individual/proto/xf86driproto-2.1.1.tar.bz2"
   sha256 "9c4b8d7221cb6dc4309269ccc008a22753698ae9245a398a59df35f1404d661f"
   # tag "linuxbrew"
 
@@ -10,8 +10,8 @@ class Xf86driproto < Formula
     sha256 "add3e319e6aa797042f7b8eb8ca39b9213fdc642330769cd80f1656b007be6fa" => :x86_64_linux
   end
 
-  depends_on "pkg-config"         =>  :build
-  depends_on "util-macros"        =>  :build
+  depends_on "pkg-config" => :build
+  depends_on "util-macros" => :build
 
   def install
     args = %W[
