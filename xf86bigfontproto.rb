6c66c2559476d8ecab9516d0bea4e413c0e4c283
Bob W. Hogg <rwhogg@linux.com>
2017-01-15 10:58:32 -0800

    xf86bigfontproto: use https URL

--- xf86bigfontproto.rb
+++ xf86bigfontproto.rb
@@ -1,7 +1,7 @@
 class Xf86bigfontproto < Formula
   desc "X.Org Protocol Headers: xf86bigfontproto"
   homepage "http://www.x.org/" ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7lib.html
-  url    "http://ftp.x.org/pub/individual/proto/xf86bigfontproto-1.2.0.tar.bz2"
+  url "https://www.x.org/archive/individual/proto/xf86bigfontproto-1.2.0.tar.bz2"
   sha256 "ba9220e2c4475f5ed2ddaa7287426b30089e4d29bd58d35fad57ba5ea43e1648"
   # tag "linuxbrew"
 
@@ -10,8 +10,8 @@ class Xf86bigfontproto < Formula
     sha256 "af0d2b1eedf5ed2a360b8c2b5dfa9d5eed671d178244e7a20fabb3f317cc437a" => :x86_64_linux
   end
 
-  depends_on "pkg-config"         =>  :build
-  depends_on "util-macros"        =>  :build
+  depends_on "pkg-config" => :build
+  depends_on "util-macros" => :build
 
   def install
     args = %W[
