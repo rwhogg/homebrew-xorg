96df83f79300f3dd39c48891db32af1889e222e9
Bob W. Hogg <rwhogg@linux.com>
2017-01-15 10:58:32 -0800

    renderproto: use https URL

--- renderproto.rb
+++ renderproto.rb
@@ -1,7 +1,7 @@
 class Renderproto < Formula
   desc "X.Org Protocol Headers: renderproto"
   homepage "http://www.x.org/" ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7lib.html
-  url    "http://ftp.x.org/pub/individual/proto/renderproto-0.11.1.tar.bz2"
+  url "https://www.x.org/archive/individual/proto/renderproto-0.11.1.tar.bz2"
   sha256 "06735a5b92b20759204e4751ecd6064a2ad8a6246bb65b3078b862a00def2537"
   # tag "linuxbrew"
 
@@ -10,8 +10,8 @@ class Renderproto < Formula
     sha256 "212f70f35f2826cf764f65c116f9a0a6182a460adac7389deb6e74ea8cd7f54d" => :x86_64_linux
   end
 
-  depends_on "pkg-config"         =>  :build
-  depends_on "util-macros"        =>  :build
+  depends_on "pkg-config" => :build
+  depends_on "util-macros" => :build
 
   def install
     args = %W[
