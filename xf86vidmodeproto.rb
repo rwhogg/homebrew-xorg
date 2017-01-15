5a48988b5c4aa31f650aeec15881643e149f15d9
Bob W. Hogg <rwhogg@linux.com>
2017-01-15 10:58:32 -0800

    xf86vidmodeproto: use https URL

--- xf86vidmodeproto.rb
+++ xf86vidmodeproto.rb
@@ -1,7 +1,7 @@
 class Xf86vidmodeproto < Formula
   desc "X.Org Protocol Headers: xf86vidmodeproto"
   homepage "http://www.x.org/" ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7lib.html
-  url    "http://ftp.x.org/pub/individual/proto/xf86vidmodeproto-2.3.1.tar.bz2"
+  url "https://www.x.org/archive/individual/proto/xf86vidmodeproto-2.3.1.tar.bz2"
   sha256 "45d9499aa7b73203fd6b3505b0259624afed5c16b941bd04fcf123e5de698770"
   # tag "linuxbrew"
 
@@ -10,8 +10,8 @@ class Xf86vidmodeproto < Formula
     sha256 "fcf020ee7ae77f134702e0ae849e7d4211e7db404431ef0a02e6076c66e753cd" => :x86_64_linux
   end
 
-  depends_on "pkg-config"         =>  :build
-  depends_on "util-macros"        =>  :build
+  depends_on "pkg-config" => :build
+  depends_on "util-macros" => :build
 
   def install
     args = %W[
