b1de99e33ac454803adbbf0c9a43775a1d918c46
Bob W. Hogg <rwhogg@linux.com>
2017-01-15 10:58:32 -0800

    compositeproto: use https URL

--- compositeproto.rb
+++ compositeproto.rb
@@ -1,7 +1,7 @@
 class Compositeproto < Formula
   desc "X.Org Protocol Headers: compositeproto"
   homepage "http://www.x.org/" ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7lib.html
-  url    "http://ftp.x.org/pub/individual/proto/compositeproto-0.4.2.tar.bz2"
+  url "https://www.x.org/archive/individual/proto/compositeproto-0.4.2.tar.bz2"
   sha256 "049359f0be0b2b984a8149c966dd04e8c58e6eade2a4a309cf1126635ccd0cfc"
   # tag "linuxbrew"
 
