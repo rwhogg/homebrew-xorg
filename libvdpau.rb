438318ded7d1e0331e68e481f6b621be486ffcf6
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-02-12 16:58:16 +0000

    libvdpau: add 1.1.1 bottle for Linuxbrew.
    Also, remove commented-out dependency on xorg
    
    Closes Linuxbrew/homebrew-xorg#258.
    Closes Linuxbrew/homebrew-xorg#261.
    
    Signed-off-by: Maxim Belkin <maxim.belkin@gmail.com>

--- libvdpau.rb
+++ libvdpau.rb
@@ -4,6 +4,10 @@ class Libvdpau < Formula
   url "https://people.freedesktop.org/~aplattner/vdpau/libvdpau-1.1.1.tar.gz"
   sha256 "5fe093302432ef05086ca2ee429c789b7bf843e166d482d166e56859b08bef55"
 
+  bottle do
+    sha256 "4f6c58d9040e7ee0a5206b7f76be36df402219ff112b6e4d69278f6ee97bb7c3" => :x86_64_linux
+  end
+
   option "without-test", "Skip compile-time tests"
   option "with-static",   "Build static libraries (not recommended)"
 
@@ -11,7 +15,6 @@ class Libvdpau < Formula
   depends_on "pkg-config" => :build
 
   # Required
-  # depends_on "xorg"
   depends_on "dri2proto"
   depends_on "libxext"
   depends_on "libx11"
