70ceec49256703ec8672ce10b103eebf2a80bc18
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-02-12 23:32:45 +0000

    glu: add 9.0.0 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-xorg#262.
    Signed-off-by: LinuxbrewTestBot <testbot@linuxbrew.sh>

--- glu.rb
+++ glu.rb
@@ -1,8 +1,13 @@
 class Glu < Formula
   desc "Mesa OpenGL Utility library"
-  homepage "http://ftp.freedesktop.org/"
+  homepage "https://cgit.freedesktop.org/mesa/glu"
   url "ftp://ftp.freedesktop.org/pub/mesa/glu/glu-9.0.0.tar.bz2"
   sha256 "1f7ad0d379a722fcbd303aa5650c6d7d5544fde83196b42a73d1193568a4df12"
+
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "7f3743002c4472585848e401c6c9631b33b8386e6c4c19b622a9c9f9afadae95" => :x86_64_linux
+  end
 
   option "with-static", "Build static libraries (not recommended)"
 
