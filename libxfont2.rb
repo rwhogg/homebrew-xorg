523d58b51a3f88ccc1c8a0d3de406dc387330f76
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-02-20 21:39:49 +0000

    libxfont2: new formula
    Closes Linuxbrew/homebrew-xorg#270.
    
    Signed-off-by: LinuxbrewTestBot <testbot@linuxbrew.sh>

---
+++ libxfont2.rb
@@ -0,0 +1,67 @@
+class Libxfont2 < Formula
+  desc "X.Org Libraries: libXfont"
+  homepage "http://www.x.org/" ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7lib.html
+  url "http://ftp.x.org/pub/individual/lib/libXfont2-2.0.1.tar.bz2"
+  sha256 "e9fbbb475ddd171b3a6a54b989cbade1f6f874fc35d505ebc5be426bc6e4db7e"
+  # tag "linuxbrew"
+
+  option "without-test", "Skip compile-time tests"
+  option "with-static", "Build static libraries (not recommended)"
+  option "with-devel-docs", "Build developer documentation"
+
+  option "with-brewed-zlib", "Use brewed zlib"
+  option "with-brewed-bzip2", "Use libbz2 to support bzip2 compressed bitmap fonts"
+
+  depends_on "pkg-config" => :build
+  depends_on "xproto"
+  depends_on "xtrans" => :build
+  depends_on "fontsproto"
+  depends_on "libfontenc"
+  depends_on "freetype"
+
+  depends_on "bzip2" if build.with?("brewed-bzip2")
+  depends_on "zlib"  if build.with?("brewed-zlib")
+
+  if build.with?("devel-docs")
+
+    patch do
+      url "https://raw.githubusercontent.com/Linuxbrew/homebrew-xorg/master/patch_configure.diff"
+      sha256 "e3aff4be9c8a992fbcbd73fa9ea6202691dd0647f73d1974ace537f3795ba15f"
+    end
+
+    depends_on "xmlto" => :build
+    depends_on "fop" => :build
+    depends_on "xorg-sgml-doctools" => [:build, :recommended]
+  end
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --sysconfdir=#{etc}
+      --localstatedir=#{var}
+      --disable-dependency-tracking
+      --disable-silent-rules
+      --enable-static=#{build.with?("static") ? "yes" : "no"}
+      --enable-devel-docs=#{build.with?("devel-docs") ? "yes" : "no"}
+      --with-freetype-config=#{Formula["freetype"].opt_bin}/freetype-config
+    ]
+
+    system "./configure", *args
+    system "make"
+    system "make", "check" if build.with?("test")
+    system "make", "install"
+  end
+
+  test do
+    output = shell_output("ldd #{lib}/libXfont2.so").chomp
+    libs = %w[
+      libfontenc.so.1
+      libfreetype.so.6
+      libpng16.so.16
+    ]
+
+    libs.each do |lib|
+      assert_match lib, output
+    end
+  end
+end
