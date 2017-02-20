02a17b2396e62361184851a664650132b3796023
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-02-20 21:40:45 +0000

    libxfont: fixes + test block
    Closes Linuxbrew/homebrew-xorg#274.
    
    Signed-off-by: LinuxbrewTestBot <testbot@linuxbrew.sh>

--- libxfont.rb
+++ libxfont.rb
@@ -17,7 +17,6 @@ class Libxfont < Formula
   option "with-brewed-zlib", "Use brewed zlib"
   option "with-brewed-bzip2", "Use libbz2 to support bzip2 compressed bitmap fonts"
 
-  # Required dependencies
   depends_on "pkg-config" => :build
   depends_on "xproto"
   depends_on "xtrans" => :build
@@ -25,19 +24,18 @@ class Libxfont < Formula
   depends_on "libfontenc"
   depends_on "freetype"
 
-  # Optional dependencies
   depends_on "bzip2" if build.with?("brewed-bzip2")
   depends_on "zlib"  if build.with?("brewed-zlib")
 
-  # Patch for xmlto
-  patch do
-    url "https://raw.githubusercontent.com/Linuxbrew/homebrew-xorg/master/patch_configure.diff"
-    sha256 "e3aff4be9c8a992fbcbd73fa9ea6202691dd0647f73d1974ace537f3795ba15f"
-  end
-
   if build.with?("devel-docs")
+
+    patch do
+      url "https://raw.githubusercontent.com/Linuxbrew/homebrew-xorg/master/patch_configure.diff"
+      sha256 "e3aff4be9c8a992fbcbd73fa9ea6202691dd0647f73d1974ace537f3795ba15f"
+    end
+
     depends_on "xmlto" => :build
-    depends_on "fop" => [:build, :recommended]
+    depends_on "fop" => :build
     depends_on "xorg-sgml-doctools" => [:build, :recommended]
   end
 
@@ -48,16 +46,27 @@ class Libxfont < Formula
       --localstatedir=#{var}
       --disable-dependency-tracking
       --disable-silent-rules
+      --enable-static=#{build.with?("static") ? "yes" : "no"}
+      --enable-devel-docs=#{build.with?("devel-docs") ? "yes" : "no"}
+      --with-freetype-config=#{Formula["freetype"].opt_bin}/freetype-config
     ]
-
-    # Be explicit about the configure flags
-    args << "--enable-static=#{build.with?("static") ? "yes" : "no"}"
-    args << "--enable-devel-docs=#{build.with?("devel-docs") ? "yes" : "no"}"
-    args << "--with-freetype-config=#{Formula["freetype"].bin}/freetype-config"
 
     system "./configure", *args
     system "make"
     system "make", "check" if build.with?("test")
     system "make", "install"
+  end
+
+  test do
+    output = shell_output("ldd #{lib}/libXfont.so").chomp
+    libs = %w[
+      libfontenc.so.1
+      libfreetype.so.6
+      libpng16.so.16
+    ]
+
+    libs.each do |lib|
+      assert_match lib, output
+    end
   end
 end
