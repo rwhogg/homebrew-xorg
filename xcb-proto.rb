da95c4b25ad3d9c02b1f4e79735baa1ccb20786e
Maxim Belkin <maxim.belkin@gmail.com>
2017-04-04 14:39:31 -0500

    xcb-proto: new version 1.12
    Closes Linuxbrew/homebrew-xorg#281.
    
    Signed-off-by: Maxim Belkin <maxim.belkin@gmail.com>

--- xcb-proto.rb
+++ xcb-proto.rb
@@ -1,8 +1,8 @@
 class XcbProto < Formula
   desc "XML-XCB protocol descriptions that libxcb uses for code generation"
-  homepage "http://www.x.org/" ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7lib.html
-  url    "https://xcb.freedesktop.org/dist/xcb-proto-1.11.tar.bz2"
-  sha256 "b4aceee6502a0ce45fc39b33c541a2df4715d00b72e660ebe8c5bb444771e32e"
+  homepage "https://www.x.org/"
+  url    "https://xcb.freedesktop.org/dist/xcb-proto-1.12.tar.bz2"
+  sha256 "5922aba4c664ab7899a29d92ea91a87aa4c1fc7eb5ee550325c3216c480a4906"
   # tag "linuxbrew"
 
   bottle do
@@ -12,15 +12,24 @@ class XcbProto < Formula
   option "without-test", "Skip compile-time tests"
   option "with-python3", "Build with python3 (default version is used otherwise)"
 
-  depends_on "pkg-config"  => :build
+  depends_on "pkg-config" => :build
+  depends_on "libxml2" => :build if build.with? "test"
 
-  if build.with?("python3")
+  if build.with? "python3"
     depends_on :python3      => :build
   else
     depends_on :python       => :build
   end
 
-  depends_on "libxml2"    => :build if build.with?("test") # to run tests
+  patch :p1 do
+    url "http://www.linuxfromscratch.org/patches/blfs/svn/xcb-proto-1.12-python3-1.patch"
+    sha256 "4eef0285392c525fcb7aeb54c1b6dc8406dc33c5845c1311b65b3efc409bea2e"
+  end
+
+  patch :p1 do
+    url "http://www.linuxfromscratch.org/patches/blfs/svn/xcb-proto-1.12-schema-1.patch"
+    sha256 "3bc1a1871d17325d1f591a8ec2091f956b2071d83a6e9998d7d4880c4abf3e8b"
+  end
 
   def install
     args = %W[
@@ -30,12 +39,11 @@ class XcbProto < Formula
       --disable-silent-rules
     ]
 
-    if build.with?("python3")
-      ENV["PYTHON"] = "python3"
-    end
+    ENV["PYTHON"] = "python3" if build.with? "python3"
+
     system "./configure", *args
     system "make"
-    system "make", "check" if build.with?("test")
+    system "make", "check" if build.with? "test"
     system "make", "install"
   end
 end
