8ad6beba739640cb08dd1b4874895b1f5c365f17
Maxim Belkin <maxim.belkin@gmail.com>
2017-04-05 09:02:06 -0500

    libxcb: new version 1.12
    Closes Linuxbrew/homebrew-xorg#282.
    
    Signed-off-by: Maxim Belkin <maxim.belkin@gmail.com>

--- libxcb.rb
+++ libxcb.rb
@@ -1,8 +1,8 @@
 class Libxcb < Formula
   desc "Interface to the X Window System protocol and replacement for Xlib"
   homepage "https://www.x.org/" ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7lib.html
-  url "https://xcb.freedesktop.org/dist/libxcb-1.11.1.tar.bz2"
-  sha256 "b720fd6c7d200e5371affdb3f049cc8f88cff9aed942ff1b824d95eedbf69d30"
+  url "https://xcb.freedesktop.org/dist/libxcb-1.12.tar.bz2"
+  sha256 "4adfb1b7c67e99bc9c2ccb110b2f175686576d2f792c8a71b9c8b19014057b5b"
   # tag "linuxbrew"
 
   bottle do
@@ -11,6 +11,7 @@ class Libxcb < Formula
   end
 
   option "without-test", "Skip compile-time tests"
+  option "with-python3", "Build with python3 (default version is used otherwise)"
   option "with-static", "Build static libraries (not recommended)"
   option "with-docs", "Generate API documentation"
 
@@ -24,15 +25,30 @@ class Libxcb < Formula
   depends_on "check" => :build if build.with? "test"
   depends_on "libxslt" => [:build, :optional]
 
+  if build.with? "python3"
+    depends_on :python3      => :build
+  else
+    depends_on :python       => :build
+  end
+
+  patch :p1 do
+    url "https://cgit.freedesktop.org/xcb/libxcb/patch/?id=8740a288ca468433141341347aa115b9544891d3"
+    sha256 "fef834cb600cdeafac052be9535f9e84e5e71d2434bdd43deef8b116a459123b"
+  end
+
   def install
     args = %W[
       --prefix=#{prefix}
       --sysconfdir=#{etc}
       --localstatedir=#{var}
+      --enable-xevie
       --enable-xinput
+      --enable-xprint
       --disable-dependency-tracking
       --disable-silent-rules
     ]
+
+    ENV["PYTHON"] = "python3" if build.with? "python3"
 
     # Be explicit about the configure flags
     args << "--enable-static=#{build.with?("static") ? "yes" : "no"}"
