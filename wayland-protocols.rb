215cc838dcf07d4b6765300d1d165d192fb4d40f
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-04-09 05:53:18 +0000

    wayland-protocols: add 1.7 bottle for Linuxbrew.

--- wayland-protocols.rb
+++ wayland-protocols.rb
@@ -4,6 +4,11 @@ class WaylandProtocols < Formula
   url "https://wayland.freedesktop.org/releases/wayland-protocols-1.7.tar.xz"
   sha256 "635f2a937d318f1fecb97b54074ca211486e38af943868dd0fa82ea38d091c1f"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "bf59b4eb86d2132ab948fe179575c92b1d4f9e2965cb21827f8537e21ca1f929" => :x86_64_linux
+  end
+
   head do
     url "git://anongit.freedesktop.org/wayland/wayland-protocols"
   end
