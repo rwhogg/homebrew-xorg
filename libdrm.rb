b244d014203eb4cca3168771eb0a8a65dddc86e8
Maxim Belkin <maxim.belkin@gmail.com>
2017-05-02 23:45:50 +0000

    libdrm: 2.4.80
    Closes Linuxbrew/homebrew-xorg#294.
    
    Signed-off-by: Maxim Belkin <maxim.belkin@gmail.com>

--- libdrm.rb
+++ libdrm.rb
@@ -1,8 +1,8 @@
 class Libdrm < Formula
   desc "libdrm: cross-driver middleware"
-  homepage "http://dri.freedesktop.org"
-  url "https://dri.freedesktop.org/libdrm/libdrm-2.4.68.tar.bz2"
-  sha256 "5b4bd9a5922929bc716411cb74061fbf31b06ba36feb89bc1358a91a8d0ca9df"
+  homepage "https://dri.freedesktop.org"
+  url "https://dri.freedesktop.org/libdrm/libdrm-2.4.80.tar.bz2"
+  sha256 "a82a519601e9cdfad795e760807eb07ac8913b225e25fc8fe9bc03e3be6549f1"
 
   bottle do
     cellar :any_skip_relocation
@@ -10,16 +10,14 @@ class Libdrm < Formula
   end
 
   option "without-test", "Skip compile-time tests"
-  option "with-static",   "Build static libraries (not recommended)"
+  option "with-static", "Build static libraries (not recommended)"
   option "with-valgrind", "Build libdrm with valgrind support"
 
   depends_on "pkg-config" => :build
-  depends_on "linuxbrew/xorg/util-macros" => :build
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
-  depends_on "libtool" => :build
   depends_on "linuxbrew/xorg/libpciaccess"
-  depends_on "linuxbrew/xorg/libpthread-stubs" => :build
+  depends_on "cunit" if build.with? "test"
+  depends_on "cairo" if build.with? "test"
+  depends_on "valgrind" => :optional
 
   def install
     args = %W[
@@ -30,16 +28,11 @@ class Libdrm < Formula
       --disable-silent-rules
       --enable-udev
     ]
-
-    # Be explicit about the configure flags
     args << "--enable-static=#{build.with?("static") ? "yes" : "no"}"
-
-    ENV["ACLOCAL"] = "aclocal -I #{HOMEBREW_PREFIX}/share/aclocal"
-    system "autoreconf", "-fiv"
 
     system "./configure", *args
     system "make"
-    system "make", "check" if build.with?("test")
+    system "make", "check" if build.with? "test"
     system "make", "install"
   end
 end
