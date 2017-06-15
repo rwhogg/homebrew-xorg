9a7f0023ed7ade883f1233a7ad2cfb6024dff0d4
Bob W. Hogg <rwhogg@linux.com>
2017-06-14 19:19:00 -0700

    libevdev: Update homepage
    Signed-off-by: Bob W. Hogg <rwhogg@linux.com>

--- libevdev.rb
+++ libevdev.rb
@@ -1,6 +1,6 @@
 class Libevdev < Formula
   desc "Wrapper library for evdev devices"
-  homepage "http://www.freedesktop.org"
+  homepage "https://www.freedesktop.org/wiki/Software/libevdev/"
   url    "https://www.freedesktop.org/software/libevdev/libevdev-1.5.2.tar.xz"
   sha256 "5ee2163656a61f5703cb5c08a05c9471ffb7b640bfbe2c55194ea50d908f629b"
   # tag "linuxbrew"
