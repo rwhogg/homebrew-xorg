cca6302c02f313c6cd0ce2068194ad2e72f85b4d
Bob W. Hogg <rwhogg@linux.com>
2017-06-14 18:29:12 -0700

    libpciaccess 0.13.5
    Signed-off-by: Bob W. Hogg <rwhogg@linux.com>
    Closes Linuxbrew/homebrew-xorg#300.

--- libpciaccess.rb
+++ libpciaccess.rb
@@ -1,8 +1,9 @@
 class Libpciaccess < Formula
   desc "X.Org Libraries: libpciaccess"
   homepage "https://www.x.org/" ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7lib.html
-  url    "http://ftp.x.org/pub/individual/lib/libpciaccess-0.13.4.tar.bz2"
-  sha256 "07f864654561e4ac8629a0ef9c8f07fbc1f8592d1b6c418431593e9ba2cf2fcf"
+  url "https://www.x.org/pub/individual/lib/libpciaccess-0.13.5.tar.bz2"
+  mirror "http://ftp.x.org/pub/individual/lib/libpciaccess-0.13.5.tar.bz2"
+  sha256 "752c54e9b3c311b4347cb50aea8566fa48eab274346ea8a06f7f15de3240b999"
   # tag "linuxbrew"
 
   bottle do
