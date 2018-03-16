722192cc789a665a44402d6ced2da256cd06d43f
Lukas, Martin <martin.lukas@merck.com>
2018-03-16 19:22:15 +0000

    xcb-proto: move patches to Patches directory
    Closes Linuxbrew/homebrew-xorg#370
    
    Signed-off-by: Maxim Belkin <maxim.belkin@gmail.com>

--- Formula/xcb-proto.rb
+++ Formula/xcb-proto.rb
@@ -19,14 +19,19 @@ class XcbProto < Formula
   depends_on "python@2" => :build unless which "python2.7"
 
   patch :p1 do
-    url "http://www.linuxfromscratch.org/patches/blfs/svn/xcb-proto-1.12-python3-1.patch"
+    # March 16, 2018
+    # http://www.linuxfromscratch.org/patches/blfs/8.2/xcb-proto-1.12-python3-1.patch
+    url "https://github.com/Linuxbrew/homebrew-xorg/master/Patches/xcb-proto-1.12-python3-1.patch"
     sha256 "4eef0285392c525fcb7aeb54c1b6dc8406dc33c5845c1311b65b3efc409bea2e"
   end
 
   patch :p1 do
-    url "http://www.linuxfromscratch.org/patches/blfs/svn/xcb-proto-1.12-schema-1.patch"
+    # March 16, 2018
+    # http://www.linuxfromscratch.org/patches/blfs/8.2/xcb-proto-1.12-schema-1.patch
+    url "https://github.com/Linuxbrew/homebrew-xorg/master/Patches/xcb-proto-1.12-schema-1.patch"
     sha256 "3bc1a1871d17325d1f591a8ec2091f956b2071d83a6e9998d7d4880c4abf3e8b"
   end
+
 
   def install
     args = %W[
