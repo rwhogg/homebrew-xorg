10219466dacb84f7cac64dab40ff397f59795b2b
Maxim Belkin <maxim.belkin@gmail.com>
2018-07-16 20:23:49 +0000

    Fix libomxil-bellagio for superenv
    Closes Linuxbrew/homebrew-xorg#298.
    Closes Linuxbrew/homebrew-xorg#378.
    
    Signed-off-by: Maxim Belkin <maxim.belkin@gmail.com>

--- Formula/libomxil-bellagio.rb
+++ Formula/libomxil-bellagio.rb
@@ -3,10 +3,7 @@ class LibomxilBellagio < Formula
   homepage "https://dri.freedesktop.org"
   url "https://downloads.sourceforge.net/project/omxil/omxil/Bellagio%200.9.3/libomxil-bellagio-0.9.3.tar.gz"
   sha256 "593c0729c8ef8c1467b3bfefcf355ec19a46dd92e31bfc280e17d96b0934d74c"
-
-  bottle do
-    sha256 "f9033e9fd118666ba8cb7cfb540cb62db69613e369e48a0c18b32c92b9b3aab0" => :x86_64_linux
-  end
+  revision 1
 
   option "without-test", "Skip compile-time tests"
   option "with-static", "Build static libraries (not recommended)"
@@ -17,21 +14,30 @@ class LibomxilBellagio < Formula
   depends_on "libtool" => :build
 
   def install
+    ENV.refurbish_args # needed for `./configure`
+
     args = %W[
       --prefix=#{prefix}
       --sysconfdir=#{etc}
       --localstatedir=#{var}
       --disable-dependency-tracking
     ]
-
-    # Be explicit about the configure flags
     args << "--enable-static=#{build.with?("static") ? "yes" : "no"}"
 
-    system "autoreconf", "-fiv"
     system "./configure", *args
     ENV.deparallelize
     system "make"
     system "make", "install"
-    system "make", "check" if build.with?("test")
+
+    if build.with? "test"
+      ["audio_effects", "resource_manager"].each do |f|
+        inreplace "test/components/#{f}/Makefile" do |s|
+          s.gsub! /^bellagio_LDADD = -lomxil-bellagio$/,
+            "bellagio_LDADD = -L$(top_srcdir)/src/.libs -lomxil-bellagio"
+        end
+      end
+      system "make", "check"
+    end
+
   end
 end
