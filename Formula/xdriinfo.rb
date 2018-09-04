f85fa8701db1420a48d1b84cc319bef2521299c5
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-09-04 13:35:15 +0000

    xdriinfo: add 1.0.6 bottle.

--- Formula/xdriinfo.rb
+++ Formula/xdriinfo.rb
@@ -5,6 +5,12 @@ class Xdriinfo < Formula
   url "https://www.x.org/pub/individual/app/xdriinfo-1.0.6.tar.bz2"
   mirror "http://ftp.x.org/pub/individual/app/xdriinfo-1.0.5.tar.bz2"
   sha256 "d9ccd2c3e87899417acc9ea1f3e319a4198112babe1dc711273584f607449d51"
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-xorg"
+    cellar :any_skip_relocation
+    sha256 "680bea023686a32b12077bc0c18b64875364bdbe08ee11c63b529fcf8a3b4a22" => :x86_64_linux
+  end
+
   # tag "linuxbrew"
 
   depends_on "pkg-config" => :build
