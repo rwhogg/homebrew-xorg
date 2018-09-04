72eb4247e31826d0547be3e62ede23d56f068895
Maxim Belkin <maxim.belkin@gmail.com>
2018-09-04 13:13:11 +0000

    xdriinfo 1.0.6
    Closes Linuxbrew/homebrew-xorg#391.
    
    Signed-off-by: Maxim Belkin <maxim.belkin@gmail.com>

--- Formula/xdriinfo.rb
+++ Formula/xdriinfo.rb
@@ -2,14 +2,15 @@ class Xdriinfo < Formula
   desc "X.Org Applications: xdriinfo"
   homepage "https://www.x.org/"
   ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7app.html
-  url "https://www.x.org/pub/individual/app/xdriinfo-1.0.5.tar.bz2"
+  url "https://www.x.org/pub/individual/app/xdriinfo-1.0.6.tar.bz2"
   mirror "http://ftp.x.org/pub/individual/app/xdriinfo-1.0.5.tar.bz2"
-  sha256 "4cba3766ef89557422062287248adeb933999071bad6f3ef9c0a478a3c680119"
+  sha256 "d9ccd2c3e87899417acc9ea1f3e319a4198112babe1dc711273584f607449d51"
   # tag "linuxbrew"
 
   depends_on "pkg-config" => :build
-  depends_on "linuxbrew/xorg/libx11"
   depends_on "linuxbrew/xorg/glproto" => :build
+  depends_on "linuxbrew/xorg/kbproto" => :build
+  depends_on "linuxbrew/xorg/libx11"
   depends_on "linuxbrew/xorg/mesa"
 
   def install
