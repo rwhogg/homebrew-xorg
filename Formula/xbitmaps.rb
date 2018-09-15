3e78b2ae3fda8d3c46bed405c7ce9f8fb7aad606
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-09-15 15:22:59 +0000

    xbitmaps: update 1.1.2 bottle.

--- Formula/xbitmaps.rb
+++ Formula/xbitmaps.rb
@@ -5,8 +5,9 @@ class Xbitmaps < Formula
   sha256 "b9f0c71563125937776c8f1f25174ae9685314cbd130fb4c2efce811981e07ee"
 
   bottle do
-    cellar :any
-    sha256 "fb234ad593ff26c68e520b1c33ad27c6668b2cd1c6a569c08f9e6608ce74d9b1" => :x86_64_linux
+    root_url "https://linuxbrew.bintray.com/bottles-xorg"
+    cellar :any_skip_relocation
+    sha256 "0bdbe7764069c18309ee18f7b8b9d5ad6b69e8ad49c7af376350f184dc7ecfac" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
