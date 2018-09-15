3101df84249b33c32c09c5b6e6f313aa86d36aac
Maxim Belkin <maxim.belkin@gmail.com>
2018-09-15 16:42:09 +0000

    iceauth: 1.0.8
    Closes Linuxbrew/homebrew-xorg#409.
    
    Signed-off-by: Maxim Belkin <maxim.belkin@gmail.com>

--- Formula/iceauth.rb
+++ Formula/iceauth.rb
@@ -2,9 +2,9 @@ class Iceauth < Formula
   desc "X.Org Applications: iceauth"
   homepage "https://www.x.org/"
   ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7app.html
-  url "https://www.x.org/pub/individual/app/iceauth-1.0.7.tar.bz2"
-  mirror "http://ftp.x.org/pub/individual/app/iceauth-1.0.7.tar.bz2"
-  sha256 "1216af2dee99b318fcf8bf9a259915273bcb37a7f1e7859af4f15d0ebf6f3f0a"
+  url "https://www.x.org/pub/individual/app/iceauth-1.0.8.tar.bz2"
+  mirror "https://ftp.x.org/pub/individual/app/iceauth-1.0.8.tar.bz2"
+  sha256 "e6ee213a217265cc76050e4293ea70b98c32dce6505c6421227efbda62ab60c6"
   # tag "linuxbrew"
 
   bottle do
@@ -15,6 +15,7 @@ class Iceauth < Formula
   depends_on "pkg-config" => :build
   depends_on "linuxbrew/xorg/xproto" => :build
   depends_on "linuxbrew/xorg/libice"
+  depends_on "splint" => [:build, :optional]
 
   def install
     args = %W[
@@ -23,6 +24,7 @@ class Iceauth < Formula
       --localstatedir=#{var}
       --disable-dependency-tracking
       --disable-silent-rules
+      --with-lint=#{build.with?("splint") ? "yes" : "no"}
     ]
 
     system "./configure", *args
