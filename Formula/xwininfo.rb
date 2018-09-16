3ee95c0e4ab468576ee0ba146301bbfc7e36fef4
Maxim Belkin <maxim.belkin@gmail.com>
2018-09-16 08:16:53 +0000

    xwininfo 1.1.4
    Closes Linuxbrew/homebrew-xorg#421.
    
    Signed-off-by: Maxim Belkin <maxim.belkin@gmail.com>

--- Formula/xwininfo.rb
+++ Formula/xwininfo.rb
@@ -2,13 +2,13 @@ class Xwininfo < Formula
   desc "X.Org Applications: xwininfo"
   homepage "https://www.x.org/"
   ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7app.html
-  url "https://www.x.org/pub/individual/app/xwininfo-1.1.3.tar.bz2"
-  mirror "http://ftp.x.org/pub/individual/app/xwininfo-1.1.3.tar.bz2"
-  sha256 "218eb0ea95bd8de7903dfaa26423820c523ad1598be0751d2d8b6a2c23b23ff8"
+  url "https://www.x.org/pub/individual/app/xwininfo-1.1.4.tar.bz2"
+  mirror "http://ftp.x.org/pub/individual/app/xwininfo-1.1.4.tar.bz2"
+  sha256 "839498aa46b496492a5c65cd42cd2e86e0da88149b0672e90cb91648f8cd5b01"
   # tag "linuxbrew"
 
   depends_on "pkg-config" => :build
-  depends_on "linuxbrew/xorg/libxcb"
+  depends_on "linuxbrew/xorg/libx11"
   depends_on "linuxbrew/xorg/xcb-util-wm"
 
   def install
