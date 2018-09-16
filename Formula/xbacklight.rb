f83c49979b1d665f7d344d6940515016545e915c
Maxim Belkin <maxim.belkin@gmail.com>
2018-09-16 08:36:23 +0000

    xbacklight 1.2.2
    Closes Linuxbrew/homebrew-xorg#415.
    
    Signed-off-by: Maxim Belkin <maxim.belkin@gmail.com>

--- Formula/xbacklight.rb
+++ Formula/xbacklight.rb
@@ -2,9 +2,9 @@ class Xbacklight < Formula
   desc "X.Org Applications: xbacklight"
   homepage "https://www.x.org/"
   ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7app.html
-  url "https://www.x.org/pub/individual/app/xbacklight-1.2.1.tar.bz2"
-  mirror "http://ftp.x.org/pub/individual/app/xbacklight-1.2.1.tar.bz2"
-  sha256 "17f6cf51a35eaa918abec36b7871d28b712c169312e22a0eaf1ffe8d6468362b"
+  url "https://www.x.org/pub/individual/app/xbacklight-1.2.2.tar.bz2"
+  mirror "http://ftp.x.org/pub/individual/app/xbacklight-1.2.2.tar.bz2"
+  sha256 "615a1d13e429ed5a0300b2f4508bc199d7cf21b6b9f088a9d8188d3bc957bf5e"
   # tag "linuxbrew"
 
   bottle do
@@ -12,11 +12,11 @@ class Xbacklight < Formula
     sha256 "3d5616b0ea4f03261672a861e577bee86cb731a956af45f64e40b95d1b53ad68" => :x86_64_linux
   end
 
-  # xcb-randr >= 1.2 xcb-atom xcb-aux xcb
   depends_on "pkg-config" => :build
+  depends_on "linuxbrew/xorg/libpthread-stubs" => :build
+  depends_on "linuxbrew/xorg/xproto" => :build
   depends_on "linuxbrew/xorg/libxcb"
   depends_on "linuxbrew/xorg/xcb-util"
-  depends_on "linuxbrew/xorg/xcb-util-image"
 
   def install
     args = %W[
