ffb4cc54a32174acd7e0560c37ee2ad3697338d3
Maxim Belkin <maxim.belkin@gmail.com>
2018-09-17 22:21:12 +0000

    libxxf86misc: add util-macros build-time dep
    Signed-off-by: Maxim Belkin <maxim.belkin@gmail.com>

--- Formula/libxxf86misc.rb
+++ Formula/libxxf86misc.rb
@@ -1,7 +1,7 @@
 class Libxxf86misc < Formula
   desc "X.Org Libraries: libXxf86misc"
   homepage "https://www.x.org/" ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7lib.html
-  url "https://www.x.org/archive//individual/lib/libXxf86misc-1.0.4.tar.gz"
+  url "https://www.x.org/archive/individual/lib/libXxf86misc-1.0.4.tar.gz"
   sha256 "63a68b2fafd03236f9b0135de21976e9194d6d811ca2fd774c13a6b4be576676"
   # tag "linuxbrew"
 
@@ -15,6 +15,7 @@ class Libxxf86misc < Formula
   option "with-static", "Build static libraries (not recommended)"
 
   depends_on "pkg-config" => :build
+  depends_on "linuxbrew/xorg/util-macros" => :build
   depends_on "linuxbrew/xorg/xf86miscproto"
   depends_on "linuxbrew/xorg/libx11"
   depends_on "linuxbrew/xorg/libxext"
