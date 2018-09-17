5125d7cf52ad763ca8496302694a69dd8e78e120
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-09-17 08:04:06 +0000

    xwininfo: add 1.1.4 bottle.

--- Formula/xwininfo.rb
+++ Formula/xwininfo.rb
@@ -5,6 +5,12 @@ class Xwininfo < Formula
   url "https://www.x.org/pub/individual/app/xwininfo-1.1.4.tar.bz2"
   mirror "http://ftp.x.org/pub/individual/app/xwininfo-1.1.4.tar.bz2"
   sha256 "839498aa46b496492a5c65cd42cd2e86e0da88149b0672e90cb91648f8cd5b01"
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-xorg"
+    cellar :any_skip_relocation
+    sha256 "af9f676fd2d58ab25cdfddeb8a3ffaa8d0efaa74840445297e81db796968f007" => :x86_64_linux
+  end
+
   # tag "linuxbrew"
 
   depends_on "pkg-config" => :build
