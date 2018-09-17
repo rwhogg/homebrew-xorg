106e859d383ebb860e4585b93138622232c688bd
Maxim Belkin <maxim.belkin@gmail.com>
2018-09-17 09:26:20 +0000

    libxcursor 1.1.15
    Closes Linuxbrew/homebrew-xorg#430.
    
    Signed-off-by: Maxim Belkin <maxim.belkin@gmail.com>

--- Formula/libxcursor.rb
+++ Formula/libxcursor.rb
@@ -1,8 +1,8 @@
 class Libxcursor < Formula
   desc "X.Org Libraries: libXcursor"
   homepage "https://www.x.org/" ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7lib.html
-  url "https://ftp.x.org/pub/individual/lib/libXcursor-1.1.14.tar.bz2"
-  sha256 "9bc6acb21ca14da51bda5bc912c8955bc6e5e433f0ab00c5e8bef842596c33df"
+  url "https://ftp.x.org/pub/individual/lib/libXcursor-1.1.15.tar.bz2"
+  sha256 "294e670dd37cd23995e69aae626629d4a2dfe5708851bbc13d032401b7a3df6b"
   # tag "linuxbrew"
 
   bottle do
@@ -13,9 +13,7 @@ class Libxcursor < Formula
   option "with-static", "Build static libraries (not recommended)"
 
   depends_on "pkg-config" => :build
-  depends_on "linuxbrew/xorg/fixesproto" => :build
-  depends_on "linuxbrew/xorg/renderproto" => :build
-  depends_on "linuxbrew/xorg/xextproto" => :build
+  depends_on "linuxbrew/xorg/util-macros" => :build
   depends_on "linuxbrew/xorg/libxrender"
   depends_on "linuxbrew/xorg/libxfixes"
   depends_on "linuxbrew/xorg/libx11"
@@ -27,14 +25,12 @@ class Libxcursor < Formula
       --localstatedir=#{var}
       --disable-dependency-tracking
       --disable-silent-rules
+      --enable-static=#{build.with?("static") ? "yes" : "no"}
     ]
-
-    # Be explicit about the configure flags
-    args << "--enable-static=#{build.with?("static") ? "yes" : "no"}"
 
     system "./configure", *args
     system "make"
-    system "make", "check" if build.with?("test")
+    system "make", "check" if build.with? "test"
     system "make", "install"
   end
 end
