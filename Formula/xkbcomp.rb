4f43baef48680bed28b0f0d7bed5f2abd3b53d4c
Maxim Belkin <maxim.belkin@gmail.com>
2018-09-17 08:20:00 +0000

    xkbcomp 1.4.2
    Closes Linuxbrew/homebrew-xorg#423.
    
    Signed-off-by: Maxim Belkin <maxim.belkin@gmail.com>

--- Formula/xkbcomp.rb
+++ Formula/xkbcomp.rb
@@ -2,9 +2,9 @@ class Xkbcomp < Formula
   desc "X.Org Applications: xkbcomp"
   homepage "https://www.x.org/"
   ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7app.html
-  url "https://www.x.org/pub/individual/app/xkbcomp-1.4.0.tar.bz2"
-  mirror "http://ftp.x.org/pub/individual/app/xkbcomp-1.4.0.tar.bz2"
-  sha256 "bc69c8748c03c5ad9afdc8dff9db11994dd871b614c65f8940516da6bf61ce6b"
+  url "https://www.x.org/pub/individual/app/xkbcomp-1.4.2.tar.bz2"
+  mirror "http://ftp.x.org/pub/individual/app/xkbcomp-1.4.2.tar.bz2"
+  sha256 "6dd8bcb9be7e85bd7294abe261b8c7b0539d2fc93e41b80fb8bd013767ce8424"
   # tag "linuxbrew"
 
   bottle do
@@ -22,7 +22,7 @@ class Xkbcomp < Formula
       --localstatedir=#{var}
       --disable-dependency-tracking
       --disable-silent-rules
-      --with-xkb-config-root=#{Formula["libx11"].share}/X11/xkb
+      --with-xkb-config-root=#{Formula["libx11"].opt_share}/X11/xkb
     ]
 
     system "./configure", *args
