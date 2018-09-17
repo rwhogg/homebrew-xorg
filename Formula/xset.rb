f23a669b4cbda1b31e8465e69a347b41834d3d6e
Maxim Belkin <maxim.belkin@gmail.com>
2018-09-17 22:35:18 +0000

    xset 1.2.4
    Closes Linuxbrew/homebrew-xorg#438.
    
    Signed-off-by: Maxim Belkin <maxim.belkin@gmail.com>

--- Formula/xset.rb
+++ Formula/xset.rb
@@ -2,17 +2,15 @@ class Xset < Formula
   desc "X.Org Applications: xset"
   homepage "https://www.x.org/"
   ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7app.html
-  url "https://www.x.org/pub/individual/app/xset-1.2.3.tar.bz2"
-  mirror "http://ftp.x.org/pub/individual/app/xset-1.2.3.tar.bz2"
-  sha256 "4382f4fb29b88647e13f3b4bc29263134270747fc159cfc5f7e3af23588c8063"
+  url "https://www.x.org/pub/individual/app/xset-1.2.4.tar.bz2"
+  mirror "http://ftp.x.org/pub/individual/app/xset-1.2.4.tar.bz2"
+  sha256 "e4fd95280df52a88e9b0abc1fee11dcf0f34fc24041b9f45a247e52df941c957"
   # tag "linuxbrew"
 
   depends_on "pkg-config" => :build
-  depends_on "linuxbrew/xorg/xproto" => :build
+  depends_on "linuxbrew/xorg/util-macros" => :build
   depends_on "linuxbrew/xorg/libxmu"
-  depends_on "linuxbrew/xorg/libx11"
-  depends_on "linuxbrew/xorg/libxext"
-  depends_on "linuxbrew/xorg/libxfontcache"
+  depends_on "linuxbrew/xorg/libxfontcache" # brings in dep on libxext and libx11
   depends_on "linuxbrew/xorg/libxxf86misc"
 
   def install
