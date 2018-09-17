04ade258e4f957ba8f06ae5dea76e61da62da550
Maxim Belkin <maxim.belkin@gmail.com>
2018-09-17 08:36:29 +0000

    libdmx 1.1.4
    Closes Linuxbrew/homebrew-xorg#425.
    
    Signed-off-by: Maxim Belkin <maxim.belkin@gmail.com>

--- Formula/libdmx.rb
+++ Formula/libdmx.rb
@@ -1,8 +1,8 @@
 class Libdmx < Formula
   desc "X.Org Libraries: libdmx"
   homepage "https://www.x.org/" ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7lib.html
-  url "https://ftp.x.org/pub/individual/lib/libdmx-1.1.3.tar.bz2"
-  sha256 "c97da36d2e56a2d7b6e4f896241785acc95e97eb9557465fd66ba2a155a7b201"
+  url "https://ftp.x.org/pub/individual/lib/libdmx-1.1.4.tar.bz2"
+  sha256 "253f90005d134fa7a209fbcbc5a3024335367c930adf0f3203e754cf32747243"
   # tag "linuxbrew"
 
   bottle do
@@ -14,10 +14,10 @@ class Libdmx < Formula
   option "with-static", "Build static libraries (not recommended)"
 
   depends_on "pkg-config" => :build
-  depends_on "linuxbrew/xorg/libx11"
-  depends_on "linuxbrew/xorg/libxext"
   depends_on "linuxbrew/xorg/xextproto" => :build
-  depends_on "linuxbrew/xorg/dmxproto" => :build
+  depends_on "linuxbrew/xorg/libx11"
+  depends_on "linuxbrew/xorg/libxext" # also brings libx11
+  depends_on "linuxbrew/xorg/dmxproto"
 
   def install
     args = %W[
@@ -26,14 +26,12 @@ class Libdmx < Formula
       --localstatedir=#{var}
       --disable-dependency-tracking
       --disable-silent-rules
+      --enable-static=#{build.with?("static") ? "yes" : "no"}
     ]
-
-    # Be explicit about the configure flags
-    args << "--enable-static=#{build.with?("static") ? "yes" : "no"}"
 
     system "./configure", *args
     system "make"
-    system "make", "check" if build.with?("test")
+    system "make", "check" if build.with? "test"
     system "make", "install"
   end
 end
