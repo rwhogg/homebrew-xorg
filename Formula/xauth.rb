1682ae35c0b7828fcc27baa69bed0b5790e7427f
Maxim Belkin <maxim.belkin@gmail.com>
2018-09-17 09:41:26 +0000

    xauth 1.0.10
    Closes Linuxbrew/homebrew-xorg#431.
    
    Signed-off-by: Maxim Belkin <maxim.belkin@gmail.com>

--- Formula/xauth.rb
+++ Formula/xauth.rb
@@ -2,16 +2,14 @@ class Xauth < Formula
   desc "X.Org Applications: xauth"
   homepage "https://www.x.org/"
   ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7app.html
-  url "https://www.x.org/pub/individual/app/xauth-1.0.9.tar.bz2"
-  mirror "http://ftp.x.org/pub/individual/app/xauth-1.0.9.tar.bz2"
-  sha256 "56ce1523eb48b1f8a4f4244fe1c3d8e6af1a3b7d4b0e6063582421b0b68dc28f"
+  url "https://www.x.org/pub/individual/app/xauth-1.0.10.tar.bz2"
+  mirror "http://ftp.x.org/pub/individual/app/xauth-1.0.10.tar.bz2"
+  sha256 "5afe42ce3cdf4f60520d1658d2b17face45c74050f39af45dccdc95e73fafc4d"
   # tag "linuxbrew"
 
   depends_on "pkg-config" => :build
-  depends_on "linuxbrew/xorg/libx11"
-  depends_on "linuxbrew/xorg/libxau"
-  depends_on "linuxbrew/xorg/libxext"
-  depends_on "linuxbrew/xorg/libxmu"
+  depends_on "linuxbrew/xorg/util-macros" => :build
+  depends_on "linuxbrew/xorg/libxmu" # brings in libxext, libx11, libxau
 
   def install
     args = %W[
