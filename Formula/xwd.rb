cb86b418877744ffff62d5add67aaa0103496e0b
Maxim Belkin <maxim.belkin@gmail.com>
2018-09-17 08:23:45 +0000

    xwd 1.0.7
    Closes Linuxbrew/homebrew-xorg#424.
    
    Signed-off-by: Maxim Belkin <maxim.belkin@gmail.com>

--- Formula/xwd.rb
+++ Formula/xwd.rb
@@ -2,9 +2,9 @@ class Xwd < Formula
   desc "X.Org Applications: xwd"
   homepage "https://www.x.org/"
   ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7app.html
-  url "https://www.x.org/pub/individual/app/xwd-1.0.6.tar.bz2"
-  mirror "http://ftp.x.org/pub/individual/app/xwd-1.0.6.tar.bz2"
-  sha256 "3bb396a2268d78de4b1c3e5237a85f7849d3434e87b3cd1f4d57eef614227d79"
+  url "https://www.x.org/pub/individual/app/xwd-1.0.7.tar.bz2"
+  mirror "http://ftp.x.org/pub/individual/app/xwd-1.0.7.tar.bz2"
+  sha256 "cd6815b8b9e0b98e284d3d732fb12162159cb9dcee4f45a7d4c0bd8b308a6794"
   # tag "linuxbrew"
 
   bottle do
@@ -13,7 +13,6 @@ class Xwd < Formula
   end
 
   depends_on "pkg-config" => :build
-  depends_on "linuxbrew/xorg/xproto" => :build
   depends_on "linuxbrew/xorg/libx11"
   depends_on "linuxbrew/xorg/libxkbfile"
 
