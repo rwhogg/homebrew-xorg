28e2b2b9d0b74cfd7d953e967dfaa9613f125695
Maxim Belkin <maxim.belkin@gmail.com>
2018-09-17 05:56:13 +0000

    xkeyboardconfig 2.24
    Closes Linuxbrew/homebrew-xorg#418.
    
    Signed-off-by: Maxim Belkin <maxim.belkin@gmail.com>

--- Formula/xkeyboardconfig.rb
+++ Formula/xkeyboardconfig.rb
@@ -1,9 +1,9 @@
 class Xkeyboardconfig < Formula
   desc "Keyboard configuration database for the X Window System"
   homepage "https://xorg.freedesktop.org"
-  url "https://xorg.freedesktop.org/archive/individual/data/xkeyboard-config/xkeyboard-config-2.20.tar.bz2"
-  mirror "ftp://ftp.x.org/pub/individual/data/xkeyboard-config/xkeyboard-config-2.20.tar.bz2"
-  sha256 "d1bfc72553c4e3ef1cd6f13eec0488cf940498b612ab8a0b362e7090c94bc134"
+  url "https://xorg.freedesktop.org/archive/individual/data/xkeyboard-config/xkeyboard-config-2.24.tar.bz2"
+  mirror "ftp://ftp.x.org/pub/individual/data/xkeyboard-config/xkeyboard-config-2.24.tar.bz2"
+  sha256 "91b18580f46b4e4ea913707f6c8d68ab5286879c3a6591462f3b9e760d3ac4d7"
 
   bottle do
     cellar :any_skip_relocation
@@ -12,9 +12,13 @@ class Xkeyboardconfig < Formula
 
   depends_on "pkg-config" => :build
   depends_on "intltool" => :build
-  depends_on "linuxbrew/xorg/libx11"
+  depends_on "gettext" => :build
+  depends_on "libxslt" => :build
 
   def install
+    # Needed by intltool (xml::parser)
+    ENV.prepend_path "PERL5LIB", "#{Formula["intltool"].libexec}/lib/perl5"
+
     args = %W[
       --prefix=#{prefix}
       --sysconfdir=#{etc}
@@ -22,6 +26,7 @@ class Xkeyboardconfig < Formula
       --disable-dependency-tracking
       --disable-silent-rules
       --with-xkb-rules-symlink=xorg
+      --disable-runtime-deps
     ]
 
     system "./configure", *args
