c1e7ef18a473bbbd567fa2620b9fcf4708abd54c
Maxim Belkin <maxim.belkin@gmail.com>
2018-09-18 06:34:51 +0000

    libdrm 2.4.94
    Closes Linuxbrew/homebrew-xorg#440.
    
    Signed-off-by: Maxim Belkin <maxim.belkin@gmail.com>

--- Formula/libdrm.rb
+++ Formula/libdrm.rb
@@ -1,8 +1,8 @@
 class Libdrm < Formula
   desc "Library for accessing the direct rendering manager"
   homepage "https://dri.freedesktop.org"
-  url "https://dri.freedesktop.org/libdrm/libdrm-2.4.82.tar.bz2"
-  sha256 "43fa2dbd422d6d41ac141272cc9855360ce4d08c7cf7f2c7bb55dfe449c4ce1c"
+  url "https://dri.freedesktop.org/libdrm/libdrm-2.4.94.tar.bz2"
+  sha256 "b73c59b0a3760502c428ba81de49cd4807657d26be5e697eba3a87dd021d16be"
 
   bottle do
     sha256 "6993d092717699c084bb6723576475183c675f9f19aa9c4e051ee19bfa892cf5" => :x86_64_linux
@@ -10,13 +10,16 @@ class Libdrm < Formula
 
   option "without-test", "Skip compile-time tests"
   option "with-static", "Build static libraries (not recommended)"
-  option "with-valgrind", "Build libdrm with valgrind support"
 
   depends_on "pkg-config" => :build
   depends_on "linuxbrew/xorg/libpciaccess"
   depends_on "cunit" => :build if build.with? "test"
   depends_on "cairo" => :build if build.with? "test"
-  depends_on "valgrind" => :optional
+  depends_on "valgrind" => [:build, :optional]
+
+  depends_on "libxslt" => :build
+  depends_on "docbook" => :build
+  depends_on "docbook-xsl" => :build
 
   def install
     args = %W[
@@ -26,8 +29,14 @@ class Libdrm < Formula
       --disable-dependency-tracking
       --disable-silent-rules
       --enable-udev
+      --enable-cairo-tests
+      --enable-manpages
+      --enable-valgrind=#{build.with?("valgrind") ? "yes" : "no"}
+      --enable-static=#{build.with?("static") ? "yes" : "no"}
     ]
-    args << "--enable-static=#{build.with?("static") ? "yes" : "no"}"
+
+    # ensure we can find the docbook XML tags
+    ENV["XML_CATALOG_FILES"] = etc/"xml/catalog"
 
     system "./configure", *args
     system "make"
