d8a15ac9d654e763c1dd539122a825aca8734b8c
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-09-19 20:18:48 +0000

    libva-intel-driver: add 2.2.0 bottle.

--- Formula/libva-intel-driver.rb
+++ Formula/libva-intel-driver.rb
@@ -4,6 +4,11 @@ class LibvaIntelDriver < Formula
   url "https://github.com/intel/intel-vaapi-driver/releases/download/2.2.0/intel-vaapi-driver-2.2.0.tar.bz2"
   sha256 "e8a5f54694eb76aad42653b591030b8a53b1513144c09a80defb3d8d8c875c18"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-xorg"
+    sha256 "4f51114a8b9992929f87b4cb7fa020cc9eca91b5a78651356b390d500fe7890e" => :x86_64_linux
+  end
+
   option "with-static", "Build static libraries (not recommended)"
 
   depends_on "pkg-config" => :build
