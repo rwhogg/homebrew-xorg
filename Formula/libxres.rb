cc04269e400f207b07d278702083acc24539fe0c
Maxim Belkin <maxim.belkin@gmail.com>
2018-09-25 17:34:10 +0000

    libxres: fix dependency order
    Signed-off-by: Maxim Belkin <maxim.belkin@gmail.com>

--- Formula/libxres.rb
+++ Formula/libxres.rb
@@ -14,8 +14,8 @@ class Libxres < Formula
   option "without-test", "Skip compile-time tests"
   option "with-static", "Build static libraries (not recommended)"
 
-  depends_on "pkg-config" => :build
   depends_on "linuxbrew/xorg/resourceproto" => :build
+  depends_on "pkg-config" => :build
   depends_on "linuxbrew/xorg/libx11"
   depends_on "linuxbrew/xorg/libxext"
 
