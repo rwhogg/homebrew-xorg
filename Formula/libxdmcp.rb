88f57639c7e6cb92ae26b183519813ae8a9e68e8
Maxim Belkin <maxim.belkin@gmail.com>
2018-09-25 17:34:09 +0000

    libxdmcp: fix dependency order
    Signed-off-by: Maxim Belkin <maxim.belkin@gmail.com>

--- Formula/libxdmcp.rb
+++ Formula/libxdmcp.rb
@@ -14,8 +14,8 @@ class Libxdmcp < Formula
   option "with-static", "Build static libraries (not recommended)"
   option "with-docs", "Build documentation"
 
-  depends_on "pkg-config" => :build
   depends_on "linuxbrew/xorg/xproto" => :build
+  depends_on "pkg-config" => :build
 
   # Patch for xmlto
   patch do
