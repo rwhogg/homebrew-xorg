735d8e642ceb0f4eb8520123c511c3a495f70569
Maxim Belkin <maxim.belkin@gmail.com>
2018-09-25 17:34:09 +0000

    libevdev: fix dependency order
    Signed-off-by: Maxim Belkin <maxim.belkin@gmail.com>

--- Formula/libevdev.rb
+++ Formula/libevdev.rb
@@ -15,10 +15,10 @@ class Libevdev < Formula
   option "without-test", "Skip compile-time tests"
   option "with-python@2", "Build with Python 2"
 
-  depends_on "pkg-config" => :build
   depends_on "check" => :build if build.with? "test"
-  depends_on "python@2" => [:build, :optional]
+  depends_on "pkg-config" => :build
   depends_on "python" => :build if build.without? "python@2"
+  depends_on "python@2" => [:build, :optional]
 
   def install
     args = %W[
