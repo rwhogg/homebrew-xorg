ec808105cf71c6ce13a25e6fb770fbd79a4dc8d7
Maxim Belkin <maxim.belkin@gmail.com>
2018-09-30 17:26:09 +0000

    libxxf86dga: reorganize configure flags, fix syntax
    Signed-off-by: Maxim Belkin <maxim.belkin@gmail.com>

--- Formula/libxxf86dga.rb
+++ Formula/libxxf86dga.rb
@@ -27,14 +27,12 @@ class Libxxf86dga < Formula
       --localstatedir=#{var}
       --disable-dependency-tracking
       --disable-silent-rules
+      --enable-static=#{build.with?("static") ? "yes" : "no"}
     ]
-
-    # Be explicit about the configure flags
-    args << "--enable-static=#{build.with?("static") ? "yes" : "no"}"
 
     system "./configure", *args
     system "make"
-    system "make", "check" if build.with?("test")
+    system "make", "check" if build.with? "test"
     system "make", "install"
   end
 end
