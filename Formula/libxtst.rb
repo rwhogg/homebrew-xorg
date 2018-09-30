a0eb69e634d0c10a9d7c66aa301a9102b4cab36f
Maxim Belkin <maxim.belkin@gmail.com>
2018-09-30 17:24:05 +0000

    libxtst: reorganize configure flags, fix syntax
    Signed-off-by: Maxim Belkin <maxim.belkin@gmail.com>

--- Formula/libxtst.rb
+++ Formula/libxtst.rb
@@ -25,7 +25,7 @@ class Libxtst < Formula
     sha256 "e3aff4be9c8a992fbcbd73fa9ea6202691dd0647f73d1974ace537f3795ba15f"
   end
 
-  if build.with?("specs")
+  if build.with? "specs"
     depends_on "xmlto" => :build
     depends_on "fop" => [:build, :recommended]
     depends_on "libxslt" => [:build, :recommended]
@@ -39,15 +39,13 @@ class Libxtst < Formula
       --localstatedir=#{var}
       --disable-dependency-tracking
       --disable-silent-rules
+      --enable-static=#{build.with?("static") ? "yes" : "no"}
+      --enable-specs=#{build.with?("specs") ? "yes" : "no"}
     ]
-
-    # Be explicit about the configure flags
-    args << "--enable-static=#{build.with?("static") ? "yes" : "no"}"
-    args << "--enable-specs=#{build.with?("specs") ? "yes" : "no"}"
 
     system "./configure", *args
     system "make"
-    system "make", "check" if build.with?("test")
+    system "make", "check" if build.with? "test"
     system "make", "install"
   end
 end
