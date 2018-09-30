5421ab2e02b45ec4238e88c866390d7ba613944b
Maxim Belkin <maxim.belkin@gmail.com>
2018-09-30 17:33:57 +0000

    xcb-util-renderutil: reorganize configure flags, fix syntax
    Signed-off-by: Maxim Belkin <maxim.belkin@gmail.com>

--- Formula/xcb-util-renderutil.rb
+++ Formula/xcb-util-renderutil.rb
@@ -1,5 +1,5 @@
 class XcbUtilRenderutil < Formula
-  desc "Additional extensions to the XCB library."
+  desc "Additional extensions to the XCB library"
   homepage "https://xcb.freedesktop.org"
   url "https://xcb.freedesktop.org/dist/xcb-util-renderutil-0.3.9.tar.bz2"
   sha256 "c6e97e48fb1286d6394dddb1c1732f00227c70bd1bedb7d1acabefdd340bea5b"
@@ -7,9 +7,9 @@ class XcbUtilRenderutil < Formula
   option "with-static", "Build static libraries (not recommended)"
   option "with-docs", "Regenerate documentation (requires doxygen)"
 
-  depends_on "pkg-config" => :build
+  depends_on "doxygen" => :build if build.with? "docs"
   depends_on "linuxbrew/xorg/util-macros" => :build
-  depends_on "doxygen" => :build if build.with?("docs")
+  depends_on "pkg-config" => :build
   depends_on "linuxbrew/xorg/libxcb"
 
   def install
@@ -19,12 +19,10 @@ class XcbUtilRenderutil < Formula
       --localstatedir=#{var}
       --disable-dependency-tracking
       --disable-silent-rules
+      --enable-static=#{build.with?("static") ? "yes" : "no"}
+      --enable-devel-docs=#{build.with?("docs") ? "yes" : "no"}
+      --with-doxygen=#{build.with?("docs") ? "yes" : "no"}
     ]
-
-    # Be explicit about the configure flags
-    args << "--enable-static=#{build.with?("static") ? "yes" : "no"}"
-    args << "--enable-devel-docs=#{build.with?("docs") ? "yes" : "no"}"
-    args << "--with-doxygen=#{build.with?("docs") ? "yes" : "no"}"
 
     system "./configure", *args
     system "make"
