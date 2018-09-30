96618e5fd53b0095059678a1849bc625fb19a399
Maxim Belkin <maxim.belkin@gmail.com>
2018-09-30 17:40:15 +0000

    xcb-util-wm: reorganize configure flags, fix syntax
    Signed-off-by: Maxim Belkin <maxim.belkin@gmail.com>

--- Formula/xcb-util-wm.rb
+++ Formula/xcb-util-wm.rb
@@ -13,9 +13,9 @@ class XcbUtilWm < Formula
   option "with-static", "Build static libraries (not recommended)"
   option "with-docs", "Regenerate documentation (requires doxygen)"
 
-  depends_on "pkg-config" => :build
+  depends_on "doxygen" => :build if build.with? "docs"
   depends_on "linuxbrew/xorg/util-macros" => :build
-  depends_on "doxygen" => :build if build.with?("docs")
+  depends_on "pkg-config" => :build
   depends_on "linuxbrew/xorg/libxcb"
 
   def install
@@ -25,19 +25,17 @@ class XcbUtilWm < Formula
       --localstatedir=#{var}
       --disable-dependency-tracking
       --disable-silent-rules
+      --enable-static=#{build.with?("static") ? "yes" : "no"}
+      --enable-devel-docs=#{build.with?("docs") ? "yes" : "no"}
+      --with-doxygen=#{build.with?("docs") ? "yes" : "no"}
     ]
-
-    # Be explicit about the configure flags
-    args << "--enable-static=#{build.with?("static") ? "yes" : "no"}"
-    args << "--enable-devel-docs=#{build.with?("docs") ? "yes" : "no"}"
-    args << "--with-doxygen=#{build.with?("docs") ? "yes" : "no"}"
 
     system "./configure", *args
     system "make"
     # Here we deviate from LFS instructions that specify
     # the following command:
     #    LD_LIBRARY_PATH=$XORG_PREFIX/lib make check
-    system "make", "check" if build.with?("test")
+    system "make", "check" if build.with? "test"
     system "make", "install"
   end
 end
