cc3dee720044dadf3dc899f5f951d5d962172c8c
Maxim Belkin <maxim.belkin@gmail.com>
2018-09-30 17:32:12 +0000

    xcb-util-cursor: reorganize configure flags, fix syntax
    Signed-off-by: Maxim Belkin <maxim.belkin@gmail.com>

--- Formula/xcb-util-cursor.rb
+++ Formula/xcb-util-cursor.rb
@@ -11,9 +11,9 @@ class XcbUtilCursor < Formula
   option "with-static", "Build static libraries (not recommended)"
   option "with-docs", "Regenerate documentation (requires doxygen)"
 
-  depends_on "pkg-config" => :build
-  depends_on "linuxbrew/xorg/util-macros" => :build
   depends_on "doxygen" => :build if build.with? "docs"
+  depends_on "linuxbrew/xorg/util-macros" => :build
+  depends_on "pkg-config" => :build
   depends_on "linuxbrew/xorg/libxcb"
   depends_on "linuxbrew/xorg/xcb-util-image"
   depends_on "linuxbrew/xorg/xcb-util-renderutil"
@@ -25,12 +25,10 @@ class XcbUtilCursor < Formula
       --localstatedir=#{var}
       --disable-dependency-tracking
       --disable-silent-rules
+      --enable-static=#{build.with?("static") ? "yes" : "no"}
+      --enable-devel-docs=#{build.with?("docs") ? "yes" : "no"}
+      --with-doxygen=#{build.with?("docs") ? "yes" : "no"}
     ]
-
-    # Be explicit about the configure flags
-    args << "--enable-static=#{build.with?("static") ? "yes" : "no"}"
-    args << "--enable-devel-docs=#{build.with?("docs") ? "yes" : "no"}"
-    args << "--with-doxygen=#{build.with?("docs") ? "yes" : "no"}"
 
     system "./configure", *args
     system "make"
