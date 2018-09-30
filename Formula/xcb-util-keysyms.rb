11c178523f7beabde02f0d94e23ee0b370ad3bfe
Maxim Belkin <maxim.belkin@gmail.com>
2018-09-30 17:33:14 +0000

    xcb-util-keysyms: reorganize configure flags, fix syntax
    Signed-off-by: Maxim Belkin <maxim.belkin@gmail.com>

--- Formula/xcb-util-keysyms.rb
+++ Formula/xcb-util-keysyms.rb
@@ -1,5 +1,5 @@
 class XcbUtilKeysyms < Formula
-  desc "Additional extensions to the XCB library."
+  desc "Additional extensions to the XCB library"
   homepage "https://xcb.freedesktop.org"
   url "https://xcb.freedesktop.org/dist/xcb-util-keysyms-0.4.0.tar.bz2"
   sha256 "0ef8490ff1dede52b7de533158547f8b454b241aa3e4dcca369507f66f216dd9"
@@ -8,9 +8,9 @@ class XcbUtilKeysyms < Formula
   option "with-static", "Build static libraries (not recommended)"
   option "with-docs", "Regenerate documentation (requires doxygen)"
 
-  depends_on "pkg-config" => :build
+  depends_on "doxygen" => :build if build.with? "docs"
   depends_on "linuxbrew/xorg/util-macros" => :build
-  depends_on "doxygen" => :build if build.with?("docs")
+  depends_on "pkg-config" => :build
   depends_on "linuxbrew/xorg/libxcb"
 
   def install
@@ -20,19 +20,17 @@ class XcbUtilKeysyms < Formula
       --localstatedir=#{var}
       --disable-dependency-tracking
       --disable-silent-rules
+      --enable-static=#{build.with?("static") ? "yes" : "no"}
+      --enable-devel-docs=#{build.with?("docs") ? "yes" : "no"}
+      --with-doxygen=#{build.with?("docs") ? "yes" : "no"}
     ]
-
-    # Be explicit about the configure flags
-    args << "--enable-static=#{build.with?("static") ? "yes" : "no"}"
-    args << "--enable-devel-docs=#{build.with?("docs") ? "yes" : "no"}"
-    args << "--with-doxygen=#{build.with?("docs") ? "yes" : "no"}"
 
     system "./configure", *args
     system "make"
     # Here we deviate from LFS instructions that specify
     # the following command:
     #    LD_LIBRARY_PATH=$XORG_PREFIX/lib make check
-    system "make", "check" if build.with?("test")
+    system "make", "check" if build.with? "test"
     system "make", "install"
   end
 end
