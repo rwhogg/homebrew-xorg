fe07978c9b40dc5a6b15e01ddf0956565f719217
Maxim Belkin <maxim.belkin@gmail.com>
2018-10-03 16:36:21 +0000

    libxfixes: fix dependencies
    Remove build-time dependency on 'xextproto'
    which is brought in by 'fixesproto'
    
    Signed-off-by: Maxim Belkin <maxim.belkin@gmail.com>

--- Formula/libxfixes.rb
+++ Formula/libxfixes.rb
@@ -13,7 +13,6 @@ class Libxfixes < Formula
   option "without-test", "Skip compile-time tests"
   option "with-static", "Build static libraries (not recommended)"
 
-  depends_on "linuxbrew/xorg/xextproto" => :build
   depends_on "pkg-config" => :build
   depends_on "linuxbrew/xorg/fixesproto"
   depends_on "linuxbrew/xorg/libx11"
