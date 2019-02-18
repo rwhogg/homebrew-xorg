21fe7716af249929ab40c3a7e0d3441253981c1a
Maxim Belkin <maxim.belkin@gmail.com>
2019-02-18 11:07:50 +0000

    xorgproto: expand tabs in the bottle block

--- Formula/xorgproto.rb
+++ Formula/xorgproto.rb
@@ -5,11 +5,11 @@ class Xorgproto < Formula
   sha256 "fee885e0512899ea5280c593fdb2735beb1693ad170c22ebcc844470eec415a0"
   head "git://anongit.freedesktop.org/git/xorg/proto/xorgproto"
 
-	bottle do
-		root_url "https://linuxbrew.bintray.com/bottles-xorg"
-		cellar :any_skip_relocation
-		sha256 "92ad849e710e96bb03cfb276e72ef74aab92291045bf487ec2d1e376d160363c" => :x86_64_linux
-	end
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-xorg"
+    cellar :any_skip_relocation
+    sha256 "92ad849e710e96bb03cfb276e72ef74aab92291045bf487ec2d1e376d160363c" => :x86_64_linux
+  end
 
   if build.head?
     depends_on "autoconf" => :build
