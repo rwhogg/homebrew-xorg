d2d8f398b342300a821e0ef26f3f63027cfa42c2
Maxim Belkin <maxim.belkin@gmail.com>
2019-02-23 20:45:46 +0000

    xcursorgen 1.0.7
    Closes #471.
    
    Signed-off-by: Maxim Belkin <maxim.belkin@gmail.com>

--- Formula/xcursorgen.rb
+++ Formula/xcursorgen.rb
@@ -2,9 +2,9 @@ class Xcursorgen < Formula
   desc "X.Org Applications: xcursorgen"
   homepage "https://www.x.org/"
   ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7app.html
-  url "https://www.x.org/pub/individual/app/xcursorgen-1.0.6.tar.bz2"
-  mirror "http://ftp.x.org/pub/individual/app/xcursorgen-1.0.6.tar.bz2"
-  sha256 "31c8910f54eb175a8a74a60e7662697467e21a8bf948220a6048a93924b3f66c"
+  url "https://www.x.org/pub/individual/app/xcursorgen-1.0.7.tar.bz2"
+  mirror "http://ftp.x.org/pub/individual/app/xcursorgen-1.0.7.tar.bz2"
+  sha256 "35b6f844b24f1776e9006c880a745728800764dbe3b327a128772b4610d8eb3d"
   # tag "linuxbrew"
 
   bottle do
