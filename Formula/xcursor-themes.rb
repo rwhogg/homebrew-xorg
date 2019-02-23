c81869e9b37e87f66a82044a0950c5562e9b2479
Maxim Belkin <maxim.belkin@gmail.com>
2019-02-23 20:43:45 +0000

    xcursor-themes 1.0.6
    Closes #470.
    
    Signed-off-by: Maxim Belkin <maxim.belkin@gmail.com>

--- Formula/xcursor-themes.rb
+++ Formula/xcursor-themes.rb
@@ -2,10 +2,10 @@ class XcursorThemes < Formula
   desc "X.Org: redglass and whiteglass animated cursor themes"
   homepage "https://www.x.org/"
   ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7app.html
-  url "https://www.x.org/archive/individual/data/xcursor-themes-1.0.5.tar.bz2"
-  mirror "https://ftp.x.org/pub/individual/data/xcursor-themes-1.0.5.tar.bz2"
-  mirror "ftp://ftp.x.org/pub/individual/data/xcursor-themes-1.0.5.tar.bz2"
-  sha256 "ae8381d39096598f7fe93fbd3ab079d93347a989f29e017f399ffcd20c8e1272"
+  url "https://www.x.org/archive/individual/data/xcursor-themes-1.0.6.tar.bz2"
+  mirror "https://ftp.x.org/pub/individual/data/xcursor-themes-1.0.6.tar.bz2"
+  mirror "ftp://ftp.x.org/pub/individual/data/xcursor-themes-1.0.6.tar.bz2"
+  sha256 "ee1ec574741293abcf66ac14ce7e74add7ac6be7deb8b38179ef010d22354999"
   # tag "linuxbrew"
 
   bottle do
