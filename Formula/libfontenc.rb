d2cad72b73d50fbeddb7ad5958f6c08b0409b4e1
Maxim Belkin <maxim.belkin@gmail.com>
2019-02-23 22:27:10 +0000

    libfontenc 1.1.4
    Closes #473.
    
    Signed-off-by: Maxim Belkin <maxim.belkin@gmail.com>

--- Formula/libfontenc.rb
+++ Formula/libfontenc.rb
@@ -1,8 +1,8 @@
 class Libfontenc < Formula
   desc "X.Org Libraries: libfontenc"
   homepage "https://www.x.org/" ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7lib.html
-  url "https://ftp.x.org/pub/individual/lib/libfontenc-1.1.3.tar.bz2"
-  sha256 "70588930e6fc9542ff38e0884778fbc6e6febf21adbab92fd8f524fe60aefd21"
+  url "https://ftp.x.org/pub/individual/lib/libfontenc-1.1.4.tar.bz2"
+  sha256 "2cfcce810ddd48f2e5dc658d28c1808e86dcf303eaff16728b9aa3dbc0092079"
   # tag "linuxbrew"
 
   bottle do
