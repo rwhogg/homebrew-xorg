e064c2fe19f1337302884ec3e35518db25fd744b
Maxim Belkin <maxim.belkin@gmail.com>
2019-02-23 20:40:17 +0000

    libxau 1.0.9
    Closes #468.
    
    Signed-off-by: Maxim Belkin <maxim.belkin@gmail.com>

--- Formula/libxau.rb
+++ Formula/libxau.rb
@@ -1,8 +1,8 @@
 class Libxau < Formula
   desc "X11 Authorization Protocol"
   homepage "https://www.x.org/" ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7lib.html
-  url "https://ftp.x.org/pub/individual/lib/libXau-1.0.8.tar.bz2"
-  sha256 "fdd477320aeb5cdd67272838722d6b7d544887dfe7de46e1e7cc0c27c2bea4f2"
+  url "https://ftp.x.org/pub/individual/lib/libXau-1.0.9.tar.bz2"
+  sha256 "ccf8cbf0dbf676faa2ea0a6d64bcc3b6746064722b606c8c52917ed00dcb73ec"
   # tag "linuxbrew"
 
   bottle do
