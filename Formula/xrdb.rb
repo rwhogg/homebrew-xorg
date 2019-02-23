a33fd8bc78be75d41852c5017fc05aefaed2ed95
Maxim Belkin <maxim.belkin@gmail.com>
2019-02-23 20:36:01 +0000

    xrdb 1.2.0
    Closes #465.
    
    Signed-off-by: Maxim Belkin <maxim.belkin@gmail.com>

--- Formula/xrdb.rb
+++ Formula/xrdb.rb
@@ -2,9 +2,9 @@ class Xrdb < Formula
   desc "X.Org Applications: xrdb"
   homepage "https://www.x.org/"
   ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7app.html
-  url "https://www.x.org/pub/individual/app/xrdb-1.1.1.tar.bz2"
-  mirror "https://ftp.x.org/pub/individual/app/xrdb-1.1.1.tar.bz2"
-  sha256 "2d23ade7cdbb487996bf77cbb32cbe9bdb34d004748a53de7a4a97660d2217b7"
+  url "https://www.x.org/pub/individual/app/xrdb-1.2.0.tar.bz2"
+  mirror "https://ftp.x.org/pub/individual/app/xrdb-1.2.0.tar.bz2"
+  sha256 "f23a65cfa1f7126040d68b6cf1e4567523edac10f8dc06f23d840d330c7c6946"
   # tag "linuxbrew"
 
   bottle do
