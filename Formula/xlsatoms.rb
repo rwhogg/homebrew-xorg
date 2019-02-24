2ec6deb251a54c3dfc4200b835678c8dbba42dd1
Maxim Belkin <maxim.belkin@gmail.com>
2019-02-24 08:54:23 +0000

    xlsatoms 1.1.3
    Closes #477.
    
    Signed-off-by: Maxim Belkin <maxim.belkin@gmail.com>

--- Formula/xlsatoms.rb
+++ Formula/xlsatoms.rb
@@ -2,9 +2,9 @@ class Xlsatoms < Formula
   desc "X.Org Applications: xlsatoms"
   homepage "https://www.x.org/"
   ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7app.html
-  url "https://www.x.org/pub/individual/app/xlsatoms-1.1.2.tar.bz2"
-  mirror "http://ftp.x.org/pub/individual/app/xlsatoms-1.1.2.tar.bz2"
-  sha256 "47e5dc7c3dbda6db2cf8c00cedac1722835c1550aa21cfdbc9ba83906694dea4"
+  url "https://www.x.org/pub/individual/app/xlsatoms-1.1.3.tar.bz2"
+  mirror "http://ftp.x.org/pub/individual/app/xlsatoms-1.1.3.tar.bz2"
+  sha256 "57868f958c263976727881f3078e55b86b4a109dc578d2b92f5c6d690850a382"
   # tag "linuxbrew"
 
   bottle do
