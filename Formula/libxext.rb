2a08480ff7e4b3842c9485746fc9814749e5283f
Maxim Belkin <maxim.belkin@gmail.com>
2019-07-14 21:47:57 +0000

    libxext 1.3.4
    Closes #505.
    
    Signed-off-by: Maxim Belkin <maxim.belkin@gmail.com>

--- Formula/libxext.rb
+++ Formula/libxext.rb
@@ -1,8 +1,8 @@
 class Libxext < Formula
   desc "X.Org Libraries: libXext"
   homepage "https://www.x.org/" ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7lib.html
-  url "https://ftp.x.org/pub/individual/lib/libXext-1.3.3.tar.bz2"
-  sha256 "b518d4d332231f313371fdefac59e3776f4f0823bcb23cf7c7305bfb57b16e35"
+  url "https://ftp.x.org/pub/individual/lib/libXext-1.3.4.tar.bz2"
+  sha256 "59ad6fcce98deaecc14d39a672cf218ca37aba617c9a0f691cac3bcd28edf82b"
   # tag "linuxbrew"
 
   bottle do
