32aee73d58d3005a7a596ccbb0e65ffd4f022798
Maxim Belkin <maxim.belkin@gmail.com>
2019-07-16 17:56:35 +0000

    setxkbmap 1.3.2
    Closes #529.
    
    Signed-off-by: Maxim Belkin <maxim.belkin@gmail.com>

--- Formula/setxkbmap.rb
+++ Formula/setxkbmap.rb
@@ -2,9 +2,8 @@ class Setxkbmap < Formula
   desc "X.Org Applications: setxkbmap"
   homepage "https://www.x.org/"
   ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7app.html
-  url "https://www.x.org/pub/individual/app/setxkbmap-1.3.1.tar.bz2"
-  mirror "http://ftp.x.org/pub/individual/app/setxkbmap-1.3.1.tar.bz2"
-  sha256 "a9ddb3963f263ba13f0ea105d8c45a531832140530217cc559587bb94f02d3e1"
+  url "https://www.x.org/pub/individual/app/setxkbmap-1.3.2.tar.bz2"
+  sha256 "8ff27486442725e50b02d7049152f51d125ecad71b7ce503cfa09d5d8ceeb9f5"
   # tag "linuxbrew"
 
   bottle do
