2c6dee36259b2622ee9b20b9bfb03d3a8e8808f5
Maxim Belkin <maxim.belkin@gmail.com>
2019-07-16 16:13:49 +0000

    libxdamage 1.1.5
    Closes #516.
    
    Signed-off-by: Maxim Belkin <maxim.belkin@gmail.com>

--- Formula/libxdamage.rb
+++ Formula/libxdamage.rb
@@ -1,8 +1,8 @@
 class Libxdamage < Formula
   desc "X.Org Libraries: libXdamage"
   homepage "https://www.x.org/" ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7lib.html
-  url "https://ftp.x.org/pub/individual/lib/libXdamage-1.1.4.tar.bz2"
-  sha256 "7c3fe7c657e83547f4822bfde30a90d84524efb56365448768409b77f05355ad"
+  url "https://ftp.x.org/pub/individual/lib/libXdamage-1.1.5.tar.bz2"
+  sha256 "b734068643cac3b5f3d2c8279dd366b5bf28c7219d9e9d8717e1383995e0ea45"
   # tag "linuxbrew"
 
   bottle do
