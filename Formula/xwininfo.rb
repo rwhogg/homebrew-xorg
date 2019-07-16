fe669d740a0830f4b005d9c61fa894d10717e84d
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-07-16 16:36:25 +0000

    xwininfo: update 1.1.5 bottle.

--- Formula/xwininfo.rb
+++ Formula/xwininfo.rb
@@ -5,9 +5,8 @@ class Xwininfo < Formula
   url "https://www.x.org/pub/individual/app/xwininfo-1.1.5.tar.bz2"
   sha256 "7a405441dfc476666c744f5fcd1bc8a75abf8b5b1d85db7b88b370982365080e"
   bottle do
-    root_url "https://linuxbrew.bintray.com/bottles-xorg"
     cellar :any_skip_relocation
-    sha256 "af9f676fd2d58ab25cdfddeb8a3ffaa8d0efaa74840445297e81db796968f007" => :x86_64_linux
+    sha256 "49bf62cd79c866f4083306f338373fb0a616014c2a8dd6ab7a589359d6c4d829" => :x86_64_linux
   end
 
   # tag "linuxbrew"
