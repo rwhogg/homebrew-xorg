b52f6a11e5a5a340d6f46b738439fa4b02a6507f
Maxim Belkin <maxim.belkin@gmail.com>
2019-07-16 18:26:06 +0000

    xauth 1.1
    Closes #531.
    
    Signed-off-by: Maxim Belkin <maxim.belkin@gmail.com>

--- Formula/xauth.rb
+++ Formula/xauth.rb
@@ -2,9 +2,8 @@ class Xauth < Formula
   desc "X.Org Applications: xauth"
   homepage "https://www.x.org/"
   ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7app.html
-  url "https://www.x.org/pub/individual/app/xauth-1.0.10.tar.bz2"
-  mirror "http://ftp.x.org/pub/individual/app/xauth-1.0.10.tar.bz2"
-  sha256 "5afe42ce3cdf4f60520d1658d2b17face45c74050f39af45dccdc95e73fafc4d"
+  url "https://www.x.org/pub/individual/app/xauth-1.1.tar.bz2"
+  sha256 "6d1dd1b79dd185107c5b0fdd22d1d791ad749ad6e288d0cdf80964c4ffa7530c"
   bottle do
     root_url "https://linuxbrew.bintray.com/bottles-xorg"
     cellar :any_skip_relocation
