33d4a7992456a0cc7bc77124e2cc143f2c5d8599
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-07-22 20:26:05 +0000

    intel-gmmlib: add 19.2.3 bottle.

--- Formula/intel-gmmlib.rb
+++ Formula/intel-gmmlib.rb
@@ -4,6 +4,11 @@ class IntelGmmlib < Formula
   url "https://github.com/intel/gmmlib/archive/intel-gmmlib-19.2.3.tar.gz"
   sha256 "60944d54a4992553f92d71ba9f8f4191d297407b564b0f24e9912b415689582e"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "634e9f1d687fc2c59a37813174b79e1c01a4058c484e93f4f40d987adca8b451" => :x86_64_linux
+  end
+
   depends_on "cmake" => :build
 
   def install
