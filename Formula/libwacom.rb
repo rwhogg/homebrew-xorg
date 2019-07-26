24b022f4b88beb3d8e75bec9adaa48878c13cb2b
Maxim Belkin <maxim.belkin@gmail.com>
2019-07-26 17:41:06 +0000

    libwacom: new formula
    Closes #544.
    
    Signed-off-by: Maxim Belkin <maxim.belkin@gmail.com>

---
+++ Formula/libwacom.rb
@@ -0,0 +1,33 @@
+class Libwacom < Formula
+  desc "Library to identify wacom tablets and their model-specific features"
+  homepage "https://github.com/linuxwacom/libwacom"
+  url "https://github.com/linuxwacom/libwacom/releases/download/libwacom-0.33/libwacom-0.33.tar.bz2"
+  sha256 "94f8a3371f30f4a38d6d2290f5160d5f5ba7a7cd6e63bee95686a67a3c19dbf2"
+
+  depends_on "libxml2" => :build
+  depends_on "pkg-config" => :build
+  depends_on "glib"
+  depends_on "gtk+"
+  depends_on "librsvg"
+  depends_on "linuxbrew/xorg/libgudev"
+
+  def install
+    system "./configure",
+           "--disable-dependency-tracking",
+           "--disable-silent-rules",
+           "--prefix=#{prefix}"
+    system "make"
+    system "make", "install"
+
+    (prefix/"test").install "test-driver"
+    (prefix/"test/tests").install Dir["test/*"].select { |f| Pathname.new(f).elf? && File.executable?(f) }
+  end
+
+  test do
+    Dir.chdir prefix/"test"
+    Dir["tests/*"].each do |t|
+      name = File.basename(t)
+      system("bash", "test-driver", "--test-name", name, "--log-file", testpath/"#{name}.log", "--trs-file", testpath/"#{name}.trs", t)
+    end
+  end
+end
