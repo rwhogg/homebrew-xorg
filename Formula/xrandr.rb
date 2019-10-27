7fdb8ecba57c34eccedef1d012c56257311dffea
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-10-27 15:35:36 +0000

    xrandr: add 1.5.1 bottle.

--- Formula/xrandr.rb
+++ Formula/xrandr.rb
@@ -4,6 +4,11 @@ class Xrandr < Formula
   ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7app.html
   url "https://www.x.org/pub/individual/app/xrandr-1.5.1.tar.xz"
   sha256 "7bc76daf9d72f8aff885efad04ce06b90488a1a169d118dea8a2b661832e8762"
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "9d9fff8263ea2e4681da6f7edc4594c1c34575066277edc5f53a81513720e6df" => :x86_64_linux
+  end
+
   # tag "linuxbrew"
 
   option "without-xkeystone", "Delete (broken) xkeystone script"
