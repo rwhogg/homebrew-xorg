f62d02006a8fd58c6006ebd2ac3dbca85a07ac25
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-01-28 20:47:02 +0000

    libva-internal: update 2.6.1 bottle.

--- Formula/libva-internal.rb
+++ Formula/libva-internal.rb
@@ -5,8 +5,9 @@ class LibvaInternal < Formula
   sha256 "6c57eb642d828af2411aa38f55dc10111e8c98976dbab8fd62e48629401eaea5"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-xorg"
     cellar :any_skip_relocation
-    sha256 "08a78620d5c6eca063c2dd00d3366671c7ad063b67a55704ba5dc500326a202d" => :x86_64_linux
+    sha256 "18fcda223fafca140f308c5e7131e9a89c16dee3a0311f7f71b2e325af5c7862" => :x86_64_linux
   end
 
   keg_only <<~EOS
