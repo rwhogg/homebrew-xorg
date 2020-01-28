4264a07f22c0aa1587af45ebf51281c46d589f52
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-01-28 20:19:06 +0000

    wayland: update 1.17.91 bottle.

--- Formula/wayland.rb
+++ Formula/wayland.rb
@@ -5,8 +5,9 @@ class Wayland < Formula
   sha256 "7dea2177db78954dc65f34952a899ec4db5e62bebdeb97d6ae3b8d2b1344a844"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-xorg"
     cellar :any_skip_relocation
-    sha256 "e0d9a0b231525483515467a848fb868395241c307d45dd0887686fbb27b4f044" => :x86_64_linux
+    sha256 "6761303c37e5f27657b6cc3632dea76465b8b273236ca8800b84914f42f6a4ed" => :x86_64_linux
   end
 
   head do
