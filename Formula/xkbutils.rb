2d5d9c0f624522b34698c7b9703d8691e72a183a
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-01-30 22:53:56 +0000

    xkbutils: add 1.0.4 bottle.

--- Formula/xkbutils.rb
+++ Formula/xkbutils.rb
@@ -6,6 +6,12 @@ class Xkbutils < Formula
   url "https://www.x.org/pub/individual/app/xkbutils-1.0.4.tar.bz2"
   mirror "http://ftp.x.org/pub/individual/app/xkbutils-1.0.4.tar.bz2"
   sha256 "d2a18ab90275e8bca028773c44264d2266dab70853db4321bdbc18da75148130"
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-xorg"
+    cellar :any_skip_relocation
+    sha256 "951a9f5e210d501b0d0b69764b0cbf01acc7a082c0a76ca27ef06e77f949b8e9" => :x86_64_linux
+  end
+
   # tag "linuxbrew"
 
   depends_on "linuxbrew/xorg/xorgproto" => :build
