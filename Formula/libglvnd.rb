fa3a1e6e9a66f7a57faea928e510a6ba89364a30
Maxim Belkin <maxim.belkin@gmail.com>
2020-01-30 23:06:13 +0000

    libglvnd 1.3.0. Switch to building with meson & ninja
    Closes #616.
    
    Signed-off-by: Maxim Belkin <maxim.belkin@gmail.com>

--- Formula/libglvnd.rb
+++ Formula/libglvnd.rb
@@ -1,8 +1,8 @@
 class Libglvnd < Formula
   desc "GL Vendor-Neutral Dispatch library"
   homepage "https://github.com/NVIDIA/libglvnd"
-  url "https://github.com/NVIDIA/libglvnd/releases/download/v1.2.0/libglvnd-1.2.0.tar.gz"
-  sha256 "2dacbcfa47b7ffb722cbddc0a4f1bc3ecd71d2d7bb461bceb8e396dc6b81dc6d"
+  url "https://github.com/NVIDIA/libglvnd/archive/v1.3.0.tar.gz"
+  sha256 "fabf98e72e172a1402617f5daade4dd79c752a77ab1688e0c1a0ffc49605040f"
   head "https://github.com/NVIDIA/libglvnd.git"
 
   bottle do
@@ -10,37 +10,31 @@ class Libglvnd < Formula
     sha256 "29da7d985d172c265c4b9573fc4a8cc6d29cf81f90f19840b18114942ff20289" => :x86_64_linux
   end
 
-  option "without-asm", "Build without assembly"
-  option "without-tls", "Build without TLS support"
-
   depends_on "linuxbrew/xorg/libpthread-stubs" => :build
   depends_on "linuxbrew/xorg/libxext" => :build
   depends_on "linuxbrew/xorg/util-macros" => :build
   depends_on "linuxbrew/xorg/xorgproto" => :build
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
   depends_on "pkg-config" => :build
   depends_on "python@2" => :build
-
-  if build.head?
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "libtool" => :build
-  end
   depends_on "linuxbrew/xorg/libx11"
 
   def install
     args = %W[
-      --prefix=#{prefix}
-      --sysconfdir=#{etc}
-      --localstatedir=#{var}
-      --disable-dependency-tracking
-      --disable-silent-rules
-      --enable-asm=#{build.with?("asm") ? "yes" : "no"}
-      --enable-tls=#{build.with?("tls") ? "yes" : "no"}
+      -Dprefix=#{prefix}
+      -Dsysconfdir=#{etc}
+      -Dlocalstatedir=#{var}
+      -Dasm=enabled
+      -Dx11=enabled
+      -Dglx=enabled
+      -Dtls=enabled
     ]
 
-    system "./autogen.sh" if build.head?
-    system "./configure", *args
-    system "make"
-    system "make", "install"
+    mkdir "build" do
+      system "meson", *args
+      system "ninja"
+      system "ninja", "install"
+    end
   end
 end
