1cf9401d0ce3cb42a26d5ac0189a2b5c9566c6f9
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-01-30 20:59:48 +0000

    libxfontcache: add 1.0.5 bottle.

--- Formula/libxfontcache.rb
+++ Formula/libxfontcache.rb
@@ -1,6 +1,12 @@
 # libxfontcache: Build a bottle for Linux
 class Libxfontcache < Formula
   desc "X.Org Libraries: libXfontCache"
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-xorg"
+    cellar :any_skip_relocation
+    sha256 "3b8897ea5bd5244076f029fe03c6bd60795e18c032a3c5aa739d053ed63eddc4" => :x86_64_linux
+  end
+
   homepage "https://www.x.org/" ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7lib.html
   url "https://www.x.org/archive//individual/lib/libXfontcache-1.0.5.tar.gz"
   sha256 "fdba75307a0983d2566554e0e9effa7079551f1b7b46e8de642d067998619659"
