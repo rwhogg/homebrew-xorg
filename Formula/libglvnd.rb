662b021d2114c1b5aa1cf7e1a2a05bf15be0ffa7
Dawid Dziurla <dawidd0811@gmail.com>
2020-02-12 17:25:37 +0000

    libglvnd: Remove Python 2 support
    Closes #624.
    
    Signed-off-by: Maxim Belkin <maxim.belkin@gmail.com>

--- Formula/libglvnd.rb
+++ Formula/libglvnd.rb
@@ -3,10 +3,10 @@ class Libglvnd < Formula
   homepage "https://github.com/NVIDIA/libglvnd"
   url "https://github.com/NVIDIA/libglvnd/archive/v1.3.0.tar.gz"
   sha256 "fabf98e72e172a1402617f5daade4dd79c752a77ab1688e0c1a0ffc49605040f"
-  head "https://github.com/NVIDIA/libglvnd.git"
+  revision 1
+  head "https://gitlab.freedesktop.org/glvnd/libglvnd.git"
 
   bottle do
-    root_url "https://linuxbrew.bintray.com/bottles-xorg"
     cellar :any_skip_relocation
     sha256 "9438f9ec28518c934b247e434e0993c3143fd789bba020ed935b4598c7b56980" => :x86_64_linux
   end
@@ -18,7 +18,6 @@ class Libglvnd < Formula
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkg-config" => :build
-  depends_on "python@2" => :build
   depends_on "linuxbrew/xorg/libx11"
 
   def install
