e888aee1b537b9f4da178fb4ae5cc647f7e331ee
Maxim Belkin <maxim.belkin@gmail.com>
2020-03-28 01:00:28 +0000

    xinput: make linuxbrew-core ready
    Signed-off-by: Maxim Belkin <maxim.belkin@gmail.com>

--- Formula/xinput.rb
+++ Formula/xinput.rb
@@ -1,14 +1,16 @@
 class Xinput < Formula
-  desc "X.Org Applications: xinput"
-  homepage "https://www.x.org/"
-  ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7app.html
+  desc "Utility to configure and test X input devices"
+  homepage "https://gitlab.freedesktop.org/xorg/app/xinput"
   url "https://www.x.org/pub/individual/app/xinput-1.6.3.tar.bz2"
   sha256 "35a281dd3b9b22ea85e39869bb7670ba78955d5fec17c6ef7165d61e5aeb66ed"
-  # tag "linuxbrew"
 
   bottle do
     cellar :any_skip_relocation
     sha256 "390c65597d14a0cb98df011aeeaaf494ccabfb6eadda0047cc0aec6d27f20cc8" => :x86_64_linux
+  end
+
+  head do
+    url "https://gitlab.freedesktop.org/xorg/app/xinput.git"
   end
 
   depends_on "linuxbrew/xorg/xorgproto" => :build
@@ -31,5 +33,11 @@ class Xinput < Formula
     system "./configure", *args
     system "make"
     system "make", "install"
+  end
+
+  test do
+    assert_predicate bin/"xinput", :exist?
+    assert_equal %Q(.TH xinput 1 "xinput #{version}" "X Version 11"),
+      shell_output("head -n 1 #{man1}/xinput.1").chomp
   end
 end
