6fe39b09c94977315018f2eddedccf63f539a098
Bob Wombat Hogg <wombat@rwhogg.site>
2020-05-09 12:49:03 -0400

    xlsclients: secure mirror
    Signed-off-by: Bob Wombat Hogg <wombat@rwhogg.site>

--- Formula/xlsclients.rb
+++ Formula/xlsclients.rb
@@ -3,7 +3,7 @@ class Xlsclients < Formula
   homepage "https://www.x.org/"
   ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7app.html
   url "https://www.x.org/pub/individual/app/xlsclients-1.1.4.tar.bz2"
-  mirror "http://ftp.x.org/pub/individual/app/xlsclients-1.1.4.tar.bz2"
+  mirror "https://ftp.x.org/pub/individual/app/xlsclients-1.1.4.tar.bz2"
   sha256 "773f2af49c7ea2c44fba4213bee64325875c1b3c9bc4bbcd8dac9261751809c1"
   # tag "linuxbrew"
 
