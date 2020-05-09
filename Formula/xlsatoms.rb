88229810fe0491093d6f5234479e1db7bfdd5d91
Bob Wombat Hogg <wombat@rwhogg.site>
2020-05-09 12:49:54 -0400

    xlsatoms: secure mirror
    Signed-off-by: Bob Wombat Hogg <wombat@rwhogg.site>

--- Formula/xlsatoms.rb
+++ Formula/xlsatoms.rb
@@ -3,7 +3,7 @@ class Xlsatoms < Formula
   homepage "https://www.x.org/"
   ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7app.html
   url "https://www.x.org/pub/individual/app/xlsatoms-1.1.3.tar.bz2"
-  mirror "http://ftp.x.org/pub/individual/app/xlsatoms-1.1.3.tar.bz2"
+  mirror "https://ftp.x.org/pub/individual/app/xlsatoms-1.1.3.tar.bz2"
   sha256 "57868f958c263976727881f3078e55b86b4a109dc578d2b92f5c6d690850a382"
   # tag "linuxbrew"
 
