35cd386117dd3c555ab018466964d8f1fb36b4e3
Bob Wombat Hogg <wombat@rwhogg.site>
2020-05-09 12:53:27 -0400

    xdriinfo: secure mirror and fix version number
    Signed-off-by: Bob Wombat Hogg <wombat@rwhogg.site>

--- Formula/xdriinfo.rb
+++ Formula/xdriinfo.rb
@@ -3,7 +3,7 @@ class Xdriinfo < Formula
   homepage "https://www.x.org/"
   ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7app.html
   url "https://www.x.org/pub/individual/app/xdriinfo-1.0.6.tar.bz2"
-  mirror "http://ftp.x.org/pub/individual/app/xdriinfo-1.0.5.tar.bz2"
+  mirror "https://ftp.x.org/pub/individual/app/xdriinfo-1.0.6.tar.bz2"
   sha256 "d9ccd2c3e87899417acc9ea1f3e319a4198112babe1dc711273584f607449d51"
   bottle do
     root_url "https://linuxbrew.bintray.com/bottles-xorg"
