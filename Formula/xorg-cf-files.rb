9a691941425016ef89b277f2ca3eaddcaae11346
Bob Wombat Hogg <wombat@rwhogg.site>
2020-05-09 12:48:23 -0400

    xorg-cf-files: secure mirror
    Signed-off-by: Bob Wombat Hogg <wombat@rwhogg.site>

--- Formula/xorg-cf-files.rb
+++ Formula/xorg-cf-files.rb
@@ -4,7 +4,7 @@ class XorgCfFiles < Formula
   ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7app.html
   url "https://www.x.org/pub/individual/util/xorg-cf-files-1.0.6.tar.bz2"
   mirror "https://www.x.org/archive/individual/util/xorg-cf-files-1.0.6.tar.bz2"
-  mirror "http://ftp.x.org/pub/individual/util/xorg-cf-files-1.0.6.tar.bz2"
+  mirror "https://ftp.x.org/pub/individual/util/xorg-cf-files-1.0.6.tar.bz2"
   sha256 "4dcf5a9dbe3c6ecb9d2dd05e629b3d373eae9ba12d13942df87107fdc1b3934d"
   # tag "linuxbrew"
 
