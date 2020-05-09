7e91920b456cd76429fa9de22864f144dfa569ea
Bob Wombat Hogg <wombat@rwhogg.site>
2020-05-09 12:48:52 -0400

    xmodmap: secure mirror
    Signed-off-by: Bob Wombat Hogg <wombat@rwhogg.site>

--- Formula/xmodmap.rb
+++ Formula/xmodmap.rb
@@ -3,7 +3,7 @@ class Xmodmap < Formula
   homepage "https://www.x.org/"
   ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7app.html
   url "https://www.x.org/pub/individual/app/xmodmap-1.0.10.tar.bz2"
-  mirror "http://ftp.x.org/pub/individual/app/xmodmap-1.0.10.tar.bz2"
+  mirror "https://ftp.x.org/pub/individual/app/xmodmap-1.0.10.tar.bz2"
   sha256 "473f0941d7439d501bb895ff358832b936ec34c749b9704c37a15e11c318487c"
   # tag "linuxbrew"
 
