863bb292555711cf8c0709654bec5a5bae72f318
Bob Wombat Hogg <wombat@rwhogg.site>
2020-05-09 12:48:12 -0400

    xvinfo: secure mirror
    Signed-off-by: Bob Wombat Hogg <wombat@rwhogg.site>

--- Formula/xvinfo.rb
+++ Formula/xvinfo.rb
@@ -3,7 +3,7 @@ class Xvinfo < Formula
   homepage "https://www.x.org/"
   ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7app.html
   url "https://www.x.org/pub/individual/app/xvinfo-1.1.4.tar.bz2"
-  mirror "http://ftp.x.org/pub/individual/app/xvinfo-1.1.4.tar.bz2"
+  mirror "https://ftp.x.org/pub/individual/app/xvinfo-1.1.4.tar.bz2"
   sha256 "0353220d6606077ba42363db65f50410759f9815352f77adc799e2adfa76e73f"
   # tag "linuxbrew"
 
