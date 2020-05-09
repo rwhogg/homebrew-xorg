d3d12c440d9ee677b2f8055144ed9dd82762b039
Bob Wombat Hogg <wombat@rwhogg.site>
2020-05-09 12:48:33 -0400

    xpr: secure mirror
    Signed-off-by: Bob Wombat Hogg <wombat@rwhogg.site>

--- Formula/xpr.rb
+++ Formula/xpr.rb
@@ -3,7 +3,7 @@ class Xpr < Formula
   homepage "https://www.x.org/"
   ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7app.html
   url "https://www.x.org/pub/individual/app/xpr-1.0.5.tar.bz2"
-  mirror "http://ftp.x.org/pub/individual/app/xpr-1.0.5.tar.bz2"
+  mirror "https://ftp.x.org/pub/individual/app/xpr-1.0.5.tar.bz2"
   sha256 "b936e0f1f8d63f5ad097a0bbdce2e67c34f5d49b7263f32a832ff62d394d1e1f"
   # tag "linuxbrew"
 
