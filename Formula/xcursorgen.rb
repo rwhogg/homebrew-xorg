43b93fdd6295cc095c3b3fa5cc340a9a1a9b54c9
Bob Wombat Hogg <wombat@rwhogg.site>
2020-05-09 12:47:32 -0400

    xcursorgen: secure mirror
    Signed-off-by: Bob Wombat Hogg <wombat@rwhogg.site>

--- Formula/xcursorgen.rb
+++ Formula/xcursorgen.rb
@@ -3,7 +3,7 @@ class Xcursorgen < Formula
   homepage "https://www.x.org/"
   ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7app.html
   url "https://www.x.org/pub/individual/app/xcursorgen-1.0.7.tar.bz2"
-  mirror "http://ftp.x.org/pub/individual/app/xcursorgen-1.0.7.tar.bz2"
+  mirror "https://ftp.x.org/pub/individual/app/xcursorgen-1.0.7.tar.bz2"
   sha256 "35b6f844b24f1776e9006c880a745728800764dbe3b327a128772b4610d8eb3d"
   # tag "linuxbrew"
 
