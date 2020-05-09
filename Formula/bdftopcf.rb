4431c546ae73b35419aa19e0ffdac3484ef0b61d
Bob Wombat Hogg <wombat@rwhogg.site>
2020-05-09 12:46:15 -0400

    bdftopcf: secure mirror
    Signed-off-by: Bob Wombat Hogg <wombat@rwhogg.site>

--- Formula/bdftopcf.rb
+++ Formula/bdftopcf.rb
@@ -3,7 +3,7 @@ class Bdftopcf < Formula
   homepage "https://www.x.org/"
   ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7app.html
   url "https://www.x.org/pub/individual/app/bdftopcf-1.1.tar.bz2"
-  mirror "http://ftp.x.org/pub/individual/app/bdftopcf-1.1.tar.bz2"
+  mirror "https://ftp.x.org/pub/individual/app/bdftopcf-1.1.tar.bz2"
   sha256 "4b4df05fc53f1e98993638d6f7e178d95b31745c4568cee407e167491fd311a2"
   # tag "linuxbrew"
 
