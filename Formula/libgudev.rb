7b710d367a28d1d2faed5c77256479a569f3a242
Bob Wombat Hogg <wombat@rwhogg.site>
2020-05-18 15:32:05 -0400

    libgudev 233
    Closes #640.
    
    Signed-off-by: Maxim Belkin <maxim.belkin@gmail.com>

--- Formula/libgudev.rb
+++ Formula/libgudev.rb
@@ -1,8 +1,8 @@
 class Libgudev < Formula
   desc "Library providing GObject bindings for libudev"
   homepage "https://wiki.gnome.org/Projects/libgudev"
-  url "https://download.gnome.org/sources/libgudev/232/libgudev-232.tar.xz"
-  sha256 "ee4cb2b9c573cdf354f6ed744f01b111d4b5bed3503ffa956cefff50489c7860"
+  url "https://download.gnome.org/sources/libgudev/233/libgudev-233.tar.xz"
+  sha256 "587c4970eb23f4e2deee2cb1fb7838c94a78c578f41ce12cac0a3f4a80dabb03"
 
   bottle do
     cellar :any_skip_relocation
@@ -15,10 +15,6 @@ class Libgudev < Formula
   depends_on "glib"
   depends_on "linuxbrew/xorg/umockdev"
   depends_on "systemd"
-
-  # https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=880332
-  # The patch has been merged upstream. It should not be necessary in the next release
-  patch :DATA
 
   def install
     ENV["XML_CATALOG_FILES"] = "#{etc}/xml/catalog"
@@ -48,27 +44,3 @@ class Libgudev < Formula
     end
   end
 end
-__END__
-diff --git a/tests/test-enumerator-filter.c b/tests/test-enumerator-filter.c
-index 9c97903..dfcdba2 100644
---- a/tests/test-enumerator-filter.c
-+++ b/tests/test-enumerator-filter.c
-@@ -37,6 +37,9 @@ test_enumerator_filter (void)
-	/* create test bed */
-	UMockdevTestbed *testbed = umockdev_testbed_new ();
-
-+	/* Relies on a test bed having been set up */
-+	g_assert (umockdev_in_mock_environment ());
-+
-	/* Add 2 devices in the USB subsystem, and one in the DRM subsystem */
-	umockdev_testbed_add_device (testbed, "usb", "dev1", NULL,
-				     "idVendor", "0815", "idProduct", "AFFE", NULL,
-@@ -74,8 +77,6 @@ int main(int argc, char **argv)
-	setlocale (LC_ALL, NULL);
-	g_test_init (&argc, &argv, NULL);
-
--	g_assert (umockdev_in_mock_environment ());
--
-	g_test_add_func ("/gudev/enumerator_filter", test_enumerator_filter);
-
-	return g_test_run ();
