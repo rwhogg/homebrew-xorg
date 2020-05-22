7c779197a741c915a147064af81e7707f411173c
Michka Popoff <michkapopoff@gmail.com>
2020-05-22 15:32:14 +0200

    wayland: revision for libffi
    Closes #641.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/wayland.rb
+++ Formula/wayland.rb
@@ -3,11 +3,11 @@ class Wayland < Formula
   homepage "https://wayland.freedesktop.org"
   url "https://wayland.freedesktop.org/releases/wayland-1.18.0.tar.xz"
   sha256 "4675a79f091020817a98fd0484e7208c8762242266967f55a67776936c2e294d"
+  revision 1
 
   bottle do
     root_url "https://linuxbrew.bintray.com/bottles-xorg"
     cellar :any_skip_relocation
-    sha256 "893fb48172f039b7b4f5f71d7f73b88b1cc6b28fae35d15c13f5153cffb3d718" => :x86_64_linux
   end
 
   head do
