0170b85e504d5c20c1a23c228d1834af95ce406e
Bob Wombat Hogg <wombat@rwhogg.site>
2020-05-31 12:16:35 -0400

    libxau: Fix Livecheckable
    Signed-off-by: Bob Wombat Hogg <wombat@rwhogg.site>

--- Livecheckables/libxau.rb
+++ Livecheckables/libxau.rb
@@ -1,4 +1,6 @@
 class Libxau
-  livecheck :url   => "https://ftp.x.org/archive/individual/lib/",
-            :regex => /libXau-([0-9.]+)\.tar.bz2/
+  livecheck do
+    url "https://ftp.x.org/archive/individual/lib/"
+    regex /libXau-([0-9.]+)\.tar.bz2/
+  end
 end
