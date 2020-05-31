891ff00cab7622015d518373db9553d7d137910c
Bob Wombat Hogg <wombat@rwhogg.site>
2020-05-31 12:12:22 -0400

    libxft: Fix Livecheckable
    Signed-off-by: Bob Wombat Hogg <wombat@rwhogg.site>

--- Livecheckables/libxft.rb
+++ Livecheckables/libxft.rb
@@ -1,4 +1,6 @@
 class Libxft
-  livecheck :url   => "https://ftp.x.org/archive/individual/lib/",
-            :regex => /libXft-([0-9.]+)\.tar.bz2/
+  livecheck do
+    url "https://ftp.x.org/archive/individual/lib/"
+    regex /libXft-([0-9.]+)\.tar.bz2/
+  end
 end
