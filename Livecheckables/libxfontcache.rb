b8ba547f04b298a1906bddecde06666ecf12b44f
Bob Wombat Hogg <wombat@rwhogg.site>
2020-05-31 12:27:29 -0400

    libxfontcache: Fix Livecheckable
    Signed-off-by: Bob Wombat Hogg <wombat@rwhogg.site>

--- Livecheckables/libxfontcache.rb
+++ Livecheckables/libxfontcache.rb
@@ -1,4 +1,6 @@
 class Libxfontcache
-  livecheck :url   => "https://ftp.x.org/archive/individual/lib/",
-            :regex => /libXfontcache-([0-9.]+)\.tar.bz2/
+  livecheck do
+    url "https://ftp.x.org/archive/individual/lib/"
+    regex /libXfontcache-([0-9.]+)\.tar.bz2/
+  end
 end
