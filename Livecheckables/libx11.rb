7418b01bb3d23e6c3a1c68f741ee0e8844b9cc78
Bob Wombat Hogg <wombat@rwhogg.site>
2020-05-31 12:20:20 -0400

    libx11: Fix Livecheckable
    Signed-off-by: Bob Wombat Hogg <wombat@rwhogg.site>

--- Livecheckables/libx11.rb
+++ Livecheckables/libx11.rb
@@ -1,4 +1,6 @@
 class Libx11
-  livecheck :url   => "https://ftp.x.org/archive/individual/lib/",
-            :regex => /libX11-([0-9.]+)\.tar.bz2/
+  livecheck do
+    url "https://ftp.x.org/archive/individual/lib/"
+    regex /libX11-([0-9.]+)\.tar.bz2/
+  end
 end
