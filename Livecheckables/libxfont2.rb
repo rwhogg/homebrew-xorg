57c6c48b827b47622bebbcdc851462cf276effbb
Bob Wombat Hogg <wombat@rwhogg.site>
2020-05-31 12:14:29 -0400

    libxfont2: Fix Livecheckable
    Signed-off-by: Bob Wombat Hogg <wombat@rwhogg.site>

--- Livecheckables/libxfont2.rb
+++ Livecheckables/libxfont2.rb
@@ -1,4 +1,6 @@
 class Libxfont2
-  livecheck :url   => "https://ftp.x.org/archive/individual/lib/",
-            :regex => /libXfont2-([0-9.]+)\.tar.bz2/
+  livecheck do
+    url "https://ftp.x.org/archive/individual/lib/"
+    regex /libXfont2-([0-9.]+)\.tar.bz2/
+  end
 end
