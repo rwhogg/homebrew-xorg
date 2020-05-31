324d6a4a73b28ed1348519d46d89d6dc91de9464
Bob Wombat Hogg <wombat@rwhogg.site>
2020-05-31 11:23:46 -0400

    libsha1: Add test

--- Formula/libsha1.rb
+++ Formula/libsha1.rb
@@ -26,4 +26,27 @@ class Libsha1 < Formula
     system "make"
     system "make", "install"
   end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <stdio.h>
+      #include <string.h>
+      #include <libsha1.h>
+
+      int main(int argc, char** argv)
+      {
+        const unsigned char* myMsg = "Hello Homebrew";
+        unsigned char hash[20];
+        sha1(hash, myMsg, strlen(myMsg));
+        for(int i = 0; i < 20; i++)
+        {
+          printf("%02x", hash[i]);
+        }
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-L#{lib}", "-I#{include}", "-lsha1", "-o", "test"
+    output = shell_output("./test").chomp
+    assert_match "9a66017dd5541865f931722de8df111c501c993d", output
+  end
 end
