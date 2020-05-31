fa3cbc5f875ebb656d17c8192fcdd5b7ed584959
Bob Wombat Hogg <wombat@rwhogg.site>
2020-05-31 12:25:09 -0400

    libxrandr: Fix Livecheckable
    Signed-off-by: Bob Wombat Hogg <wombat@rwhogg.site>

--- Livecheckables/libxrandr.rb
+++ Livecheckables/libxrandr.rb
@@ -1,4 +1,6 @@
 class Libxrandr
-  livecheck :url   => "https://ftp.x.org/archive/individual/lib/",
-            :regex => /libXrandr-([0-9.]+)\.tar.bz2/
+  livecheck do
+    url "https://ftp.x.org/archive/individual/lib/"
+    regex /libXrandr-([0-9.]+)\.tar.bz2/
+  end
 end
