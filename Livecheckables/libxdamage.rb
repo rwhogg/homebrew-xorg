e4a591ce5591f2058310ddeb669a54b8730be980
Bob Wombat Hogg <wombat@rwhogg.site>
2020-05-31 12:24:42 -0400

    libxdamage: Fix Livecheckable
    Signed-off-by: Bob Wombat Hogg <wombat@rwhogg.site>

--- Livecheckables/libxdamage.rb
+++ Livecheckables/libxdamage.rb
@@ -1,4 +1,6 @@
 class Libxdamage
-  livecheck :url   => "https://ftp.x.org/archive/individual/lib/",
-            :regex => /libXdamage-([0-9.]+)\.tar.bz2/
+  livecheck do
+    url "https://ftp.x.org/archive/individual/lib/"
+    regex /libXdamage-([0-9.]+)\.tar.bz2/
+  end
 end
