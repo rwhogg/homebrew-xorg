7e427414be1908c1e9a6135b458fed0960efe025
Bob Wombat Hogg <wombat@rwhogg.site>
2020-05-31 12:10:45 -0400

    libxi: Fix Livecheckable
    Signed-off-by: Bob Wombat Hogg <wombat@rwhogg.site>

--- Livecheckables/libxi.rb
+++ Livecheckables/libxi.rb
@@ -1,4 +1,6 @@
 class Libxi
-  livecheck :url   => "https://ftp.x.org/archive/individual/lib/",
-            :regex => /libXi-([0-9.]+)\.tar.bz2/
+  livecheck do
+    url "https://ftp.x.org/archive/individual/lib/"
+    regex /libXi-([0-9.]+)\.tar.bz2/
+  end
 end
