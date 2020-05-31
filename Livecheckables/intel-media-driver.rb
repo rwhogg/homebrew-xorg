c93303ec8cf6cf867b5a2b4486e69e817dc0837b
Bob Wombat Hogg <wombat@rwhogg.site>
2020-05-31 12:09:28 -0400

    intel-media-driver: Fix Livecheckable
    Signed-off-by: Bob Wombat Hogg <wombat@rwhogg.site>

--- Livecheckables/intel-media-driver.rb
+++ Livecheckables/intel-media-driver.rb
@@ -1,4 +1,6 @@
 class IntelMediaDriver
-  livecheck :url   => "https://github.com/intel/media-driver/releases",
-            :regex => %r{Latest.*?href="/intel/media-driver/archive/intel-media-?([a-z0-9\.]+).tar.gz}m
+  livecheck do
+    url "https://github.com/intel/media-driver/releases"
+    regex(%r{Latest.*?href="/intel/media-driver/archive/intel-media-?([a-z0-9\.]+).tar.gz}m)
+  end
 end
