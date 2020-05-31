04f74fefac2f93d4ebc3d5a71e45c0310bbb22bd
Bob Wombat Hogg <wombat@rwhogg.site>
2020-05-31 12:24:21 -0400

    libxshmfence: Fix Livecheckable
    Signed-off-by: Bob Wombat Hogg <wombat@rwhogg.site>

--- Livecheckables/libxshmfence.rb
+++ Livecheckables/libxshmfence.rb
@@ -1,4 +1,6 @@
 class Libxshmfence
-  livecheck :url   => "https://ftp.x.org/archive/individual/lib/",
-            :regex => /libxshmfence-([0-9.]+)\.tar.bz2/
+  livecheck do
+    url "https://ftp.x.org/archive/individual/lib/"
+    regex /libxshmfence-([0-9.]+)\.tar.bz2/
+  end
 end
