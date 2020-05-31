5694c1b90480ecf115d62dd1439f050130349a77
Bob Wombat Hogg <wombat@rwhogg.site>
2020-05-31 12:11:45 -0400

    libxfixes: Fix Livecheckable
    Signed-off-by: Bob Wombat Hogg <wombat@rwhogg.site>

--- Livecheckables/libxfixes.rb
+++ Livecheckables/libxfixes.rb
@@ -1,4 +1,6 @@
 class Libxfixes
-  livecheck :url   => "https://ftp.x.org/archive/individual/lib/",
-            :regex => /libXfixes-([0-9.]+)\.tar.bz2/
+  livecheck do
+    url "https://ftp.x.org/archive/individual/lib/"
+    regex /libXfixes-([0-9.]+)\.tar.bz2/
+  end
 end
