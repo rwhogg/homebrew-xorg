31c26cc94cbb5ff7991e68211df5e561a3393765
Bob Wombat Hogg <wombat@rwhogg.site>
2020-05-31 12:19:13 -0400

    libxtst: Fix Livecheckable
    Signed-off-by: Bob Wombat Hogg <wombat@rwhogg.site>

--- Livecheckables/libxtst.rb
+++ Livecheckables/libxtst.rb
@@ -1,4 +1,6 @@
 class Libxtst
-  livecheck :url   => "https://ftp.x.org/archive/individual/lib/",
-            :regex => /libXtst-([0-9.]+)\.tar.bz2/
+  livecheck do
+    url "https://ftp.x.org/archive/individual/lib/"
+    regex /libXtst-([0-9.]+)\.tar.bz2/
+  end
 end
