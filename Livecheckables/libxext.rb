9a1c73dbbd1c437ad66fa6eeb1730b9276c2f7f2
Bob Wombat Hogg <wombat@rwhogg.site>
2020-05-31 12:17:36 -0400

    libxext: Fix Livecheckable
    Signed-off-by: Bob Wombat Hogg <wombat@rwhogg.site>

--- Livecheckables/libxext.rb
+++ Livecheckables/libxext.rb
@@ -1,4 +1,6 @@
 class Libxext
-  livecheck :url   => "https://ftp.x.org/archive/individual/lib/",
-            :regex => /libXext-([0-9.]+)\.tar.bz2/
+  livecheck do
+    url "https://ftp.x.org/archive/individual/lib/"
+    regex /libXext-([0-9.]+)\.tar.bz2/
+  end
 end
