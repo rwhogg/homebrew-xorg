4b30b3c1f1aef7b6853f00ccf7cad7c22b962137
Maxim Belkin <maxim.belkin@gmail.com>
2020-06-23 21:13:49 +0000

    libx11: add a test

--- Formula/libx11.rb
+++ Formula/libx11.rb
@@ -49,4 +49,41 @@ class Libx11 < Formula
     system "make", "check" if build.with? "test"
     system "make", "install"
   end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <X11/Xlib.h>
+      #include <stdio.h>
+      int main() {
+        Display* disp = XOpenDisplay(NULL);
+        if (disp == NULL)
+        {
+          fprintf(stderr, "Unable to connect to display\\n");
+          return 7;
+        }
+
+        int screen_num = DefaultScreen(disp);
+        unsigned long background = BlackPixel(disp, screen_num);
+        unsigned long border = WhitePixel(disp, screen_num);
+        int width = 60, height = 40;
+        Window win = XCreateSimpleWindow(disp, DefaultRootWindow(disp), 0, 0, width, height, 2, border, background);
+        XSelectInput(disp, win, ButtonPressMask|StructureNotifyMask);
+        XMapWindow(disp, win); // display blank window
+
+        XGCValues values;
+        values.foreground = WhitePixel(disp, screen_num);
+        values.line_width = 1;
+        values.line_style = LineSolid;
+        GC pen = XCreateGC(disp, win, GCForeground|GCLineWidth|GCLineStyle, &values);
+        // draw two diagonal lines
+        XDrawLine(disp, win, pen, 0, 0, width, height);
+        XDrawLine(disp, win, pen, width, 0, 0, height);
+
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-o", "test", "-I#{include}", "-lX11"
+    system "./test"
+    assert_equal 0, $CHILD_STATUS.exitstatus
+  end
 end
