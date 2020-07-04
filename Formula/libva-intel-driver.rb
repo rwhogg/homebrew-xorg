f2a0515632c354006d59ae4482204319476689c9
Sam Ford <1584702+samford@users.noreply.github.com>
2020-07-04 12:14:11 -0400

    libva-intel-driver: integrate livecheckable

--- Formula/libva-intel-driver.rb
+++ Formula/libva-intel-driver.rb
@@ -4,6 +4,11 @@ class LibvaIntelDriver < Formula
   url "https://github.com/intel/intel-vaapi-driver/releases/download/2.4.0/intel-vaapi-driver-2.4.0.tar.bz2"
   sha256 "71e2ddd985af6b221389db1018c4e8ca27a7f939fb51dcdf49d0efcb5ff3d089"
 
+  livecheck do
+    url "https://github.com/intel/intel-vaapi-driver/releases"
+    regex(%r{Latest.*?href="/intel/intel-vaapi-driver/tree/v?([a-z0-9.]+)}m)
+  end
+
   bottle do
     sha256 "5d6cb7e16619d29f46d47d218304b8104f10c01b139470e298d98d6cc46faec5" => :x86_64_linux
   end
